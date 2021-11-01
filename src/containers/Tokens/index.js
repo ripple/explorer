db66d56baab3c00dfcb475923724ebab93ce8234
Mayukha Vadari <mvadari@ripple.com>
2021-11-01 18:30:27 -0400

    remove useref

--- src/containers/Tokens/index.js
+++ src/containers/Tokens/index.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import axios from 'axios';
 import { translate } from 'react-i18next';
