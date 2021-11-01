a1e438879e19f13af80eef16598d0964ffe49ac2
Mukul Jangid <49061120+mukulljangid@users.noreply.github.com>
2021-11-01 17:07:23 -0400

    fetch error from API response

--- src/containers/Tokens/index.js
+++ src/containers/Tokens/index.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import PropTypes from 'prop-types';
 import axios from 'axios';
 import { translate } from 'react-i18next';
@@ -8,6 +8,7 @@ import Log from '../shared/log';
 import TokensHeader from './TokensHeader';
 import TokensTable from './TokensTable';
 import './styles.css';
+import NoMatch from '../NoMatch';
 
 const TOP_TOKENS_URL = '/api/v1/token/top';
 
@@ -15,6 +16,7 @@ const Tokens = props => {
   const [allTokens, setAllTokens] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
   const [isError, setIsError] = useState(false);
+  const errorInfoRef = useRef({ title: '', hints: '' });
 
   const { t, error } = props;
 
@@ -25,6 +27,17 @@ const Tokens = props => {
       .get(TOP_TOKENS_URL)
       .then(res => {
         const { tokens } = res.data;
+
+        if (res.data.result === 'error') {
+          Log.error(`${TOP_TOKENS_URL} --- ${JSON.stringify(res.data)}`);
+
+          analytics(ANALYTIC_TYPES.exception, {
+            exDescription: `${TOP_TOKENS_URL} --- ${JSON.stringify(res.data)}`,
+          });
+
+          errorInfoRef.current = res.data;
+          setIsError(true);
+        }
         setAllTokens(tokens);
         setIsLoading(false);
       })
@@ -40,8 +53,13 @@ const Tokens = props => {
       });
   }, []);
 
-  return error ? (
-    Tokens.renderError(error)
+  function renderError(errorInfo) {
+    const { result, message } = errorInfo;
+    return <NoMatch title={result} hints={[message]} />;
+  }
+
+  return error || isError ? (
+    renderError(errorInfoRef.current)
   ) : (
     <div className="token-discovery-page">
       <TokensHeader tokens={allTokens} isLoading={isLoading} isError={isError} />
