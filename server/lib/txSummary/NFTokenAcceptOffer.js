bd8b25bd1c87fda56e1b08437c4861c7ce49c010
ledhed2222 <ledhed2222@users.noreply.github.com>
2021-12-15 04:53:26 -0500

    syntax cleanup

--- server/lib/txSummary/NFTokenAcceptOffer.js
+++ server/lib/txSummary/NFTokenAcceptOffer.js
@@ -3,13 +3,13 @@ const formatAmount = require('./formatAmount');
 module.exports = (tx, meta) => {
   const acceptedOfferNode = meta.AffectedNodes.find(
     node => node.DeletedNode != null && node.DeletedNode.LedgerEntryType === 'NFTokenOffer'
-  ).DeletedNode;
+  ).DeletedNode.FinalFields;
 
-  const amount = formatAmount(acceptedOfferNode.FinalFields.Amount);
-  const tokenID = acceptedOfferNode.FinalFields.TokenID;
-  const offerer = acceptedOfferNode.FinalFields.Owner;
+  const amount = formatAmount(acceptedOfferNode.Amount);
+  const tokenID = acceptedOfferNode.TokenID;
+  const offerer = acceptedOfferNode.Owner;
   const accepter = tx.Account;
-  const isSellOffer = acceptedOfferNode.FinalFields.Flags & (1 !== 0);
+  const isSellOffer = (acceptedOfferNode.Flags & 1) !== 0;
   const seller = isSellOffer ? offerer : accepter;
   const buyer = isSellOffer ? accepter : offerer;
 
