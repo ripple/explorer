6a034d5c2c27919b403ba53ebcd4b3525d9bde92
Mayukha Vadari <mvadari@ripple.com>
2022-01-12 18:25:27 -0500

    remove unneeded code

--- src/rippled/index.js
+++ src/rippled/index.js
@@ -7,9 +7,6 @@ import getQuorum from './quorum';
 import getNegativeUNL from './nUNL';
 import getToken from './token';
 import getOffers from './offers';
-import getValidators from './validators';
-import getNodes from './nodes';
-import getValidatorReport from './validatorReport';
 
 export {
   getAccountState,
@@ -21,7 +18,4 @@ export {
   getNegativeUNL,
   getToken,
   getOffers,
-  getValidators,
-  getValidatorReport,
-  getNodes,
 };
