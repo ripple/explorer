05bf421a1cd4b895c40da74be255333e55cd3386
Caleb Kniffen <ckniffen@ripple.com>
2022-04-14 12:36:51 -0500

    fix: Covert hex currency codes (#88)
    If currency code is 40 characters convert it to ASCII

---
+++ src/containers/shared/components/test/Currency.test.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import { BrowserRouter } from "react-router-dom";
+import { mount } from 'enzyme';
+import Currency from "../Currency";
+
+describe('Currency', () => {
+  it('handles currency codes that are 3 characters ', () => {
+    const wrapper = mount(
+      <Currency currency={"BTC"} amount={5.001} />
+    );
+    expect(wrapper.find('.currency').text()).toEqual('BTC');
+    wrapper.unmount();
+  });
+
+  it('handles currency codes that are 4 characters ', () => {
+    const wrapper = mount(
+      <Currency currency={"WOOT"} amount={5.001} />
+    );
+    expect(wrapper.find('.currency').text()).toEqual('WOOT');
+    wrapper.unmount();
+  });
+
+  it('handles currency codes that are 40 characters ', () => {
+    const wrapper = mount(
+      <Currency currency="584D455441000000000000000000000000000000" amount={5.001} />
+    );
+    expect(wrapper.find('.currency').text()).toEqual('XMETA');
+    wrapper.unmount();
+  });
+
+  it('handles currency codes that are 40 characters and issuer ', () => {
+    const wrapper = mount(
+      <BrowserRouter>
+        <Currency currency="584D455441000000000000000000000000000000" issuer="r3XwJ1hr1PtbRvbhuUkybV6tmYzzA11WcB" amount={5.001} />
+      </BrowserRouter>
+    );
+    expect(wrapper.find('.currency').text()).toEqual('XMETA.r3XwJ1hr1PtbRvbhuUkybV6tmYzzA11WcB');
+    wrapper.unmount();
+  });
+});
