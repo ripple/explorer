559a993ef6f7eff0f18d49e153c86ae5b9346f46
Caleb Kniffen <ckniffen@ripple.com>
2022-04-18 12:06:19 -0500

    feat: Add gzipping and cache busting to assets
    When running server in prod mode all static assets will be gzipped.
    Additionally all js and css will have versioned hashes and set cache headers.

--- build-non-split.js
+++ build-non-split.js
@@ -14,6 +14,6 @@ config.optimization.splitChunks = {
 config.optimization.runtimeChunk = false;
 
 // JS
-config.output.filename = 'static/js/[name].js';
+config.output.filename = 'static/js/[name].[hash].js';
 // CSS. "5" is MiniCssPlugin
-config.plugins[5].options.filename = 'static/css/[name].css';
+config.plugins[5].options.filename = 'static/css/[name].[hash].css';
