ripple/explorer

commit 5f4d6ae9ca664796dbadc0a5dbeb106fe5f1783b

refactor(table): Define global table styles to help future table usage (#165)
Updates:

- Validators Table
- Nodes Table
- Tokens Table

src/containers/Network/ValidatorsTable.jsx
@@ -85,10 +85,10 @@ class ValidatorsTable extends Component {
 
     return (
       <tr key={pubkey}>
-        <td className="td-pubkey" title={pubkey}>
+        <td className="pubkey text-truncate" title={pubkey}>
           <Link to={`/validators/${pubkey}`}>{pubkey}</Link>
         </td>
-        <td className="td-domain">{ValidatorsTable.renderDomain(d.domain)}</td>
+        <td className="domain text-truncate">{ValidatorsTable.renderDomain(d.domain)}</td>
         <td className={`unl ${trusted}`}>{d.unl && <SuccessIcon title={d.unl} alt={d.unl} />}</td>
         <td className={`nUnl ${nUnl}`}>
           {onNegativeUnl && <img src={infoOrange} title={d.unl} alt={d.unl} />}
@@ -113,8 +113,8 @@ class ValidatorsTable extends Component {
     const { t } = this.props;
     const { validators } = this.state;
     const content = validators ? (
-      <table>
-        <tbody>
+      <table className="basic">
+        <thead>
           <tr>
             <th className="pubkey">{t('pubkey')}</th>
             <th className="domain">{t('domain')}</th>
@@ -126,7 +126,7 @@ class ValidatorsTable extends Component {
             <th className="fee">{t('fee')}</th>
             <th className="last-ledger">{t('ledger')}</th>
           </tr>
-        </tbody>
+        </thead>
         <tbody>{validators.map(this.renderValidator)}</tbody>
       </table>
     ) : (

src/containers/Network/NodesTable.jsx
@@ -107,10 +107,8 @@ class NodesTable extends Component {
     const { ledgerRange } = this.state;
     return (
       <tr key={node.pubkey_node}>
-        <td className="pubkey">
-          <div className="title">{node.pubkey_node}</div>
-        </td>
-        <td className="host">{node.host}</td>
+        <td className="pubkey text-truncate">{node.pubkey_node}</td>
+        <td className="host text-truncate">{node.host}</td>
         <td className="state center">
           <span className={node.server_state}>{node.server_state}</span>
         </td>
@@ -137,7 +135,7 @@ class NodesTable extends Component {
     const { t } = this.props;
     const { nodes } = this.state;
     const content = nodes ? (
-      <table>
+      <table className="basic">
         <thead>
           <tr>
             <th className="pubkey">{t('node_pubkey')}</th>

src/containers/Network/css/nodesTable.scss
@@ -1,76 +1,33 @@
 @import '../../shared/css/variables.scss';
+@import '../../shared/css/table.scss';
 
 .nodes-table {
   position: relative;
   min-height: 150px;
 
   table {
-    overflow: hidden;
-    width: 100%;
-    border-top: none;
-    margin: auto;
-    border-collapse: collapse;
-    color: $gray-400;
-    font-size: 13px;
-    letter-spacing: 0px;
-    text-align: left;
-
-    tr {
-      border-bottom: 1px solid $gray-800;
-    }
-
-    th {
-      padding: 22px 5px;
-      color: $white;
-      font-size: 11px;
-      text-align: left;
-      text-transform: uppercase;
-    }
-
-    td {
-      padding: 15px 5px;
-    }
-
-    .right {
-      text-align: right;
-    }
-
-    .center {
-      text-align: center;
-    }
-
-    .pubkey div {
-      overflow: hidden;
-      width: 70px;
-      padding: 2px;
-      font-size: 11px;
-      text-overflow: ellipsis;
-      white-space: nowrap;
+    .pubkey {
+      max-width: 70px;
 
       @include for-size(tablet-portrait-up) {
-        width: 100px;
+        max-width: 100px;
       }
 
       @include for-size(big-desktop-up) {
-        width: 180px;
+        max-width: 180px;
       }
 
       @media (min-width: 1300px) {
-        width: 280px;
+        max-width: 280px;
       }
 
       @media (min-width: 1400px) {
-        width: 350px;
+        max-width: 350px;
       }
-    }
-
-    .host {
-      font-size: 11px;
     }
 
     .state {
       min-width: 60px;
-      font-size: 11px;
       text-transform: uppercase;
       @include semibold;
     }

src/containers/Network/css/validatorsTable.scss
@@ -5,75 +5,43 @@
   min-height: 150px;
 
   table {
-    width: 100%;
-    border-bottom: 1px solid $gray-800;
-    margin: auto;
-    border-collapse: collapse;
-    font-size: 13px;
-
-    a {
-      color: inherit;
-      text-decoration: none;
+    .pubkey,
+    .score.h1,
+    .score.d30,
+    .fee {
+      display: none;
     }
 
-    th {
-      padding: 22px 10px;
-      border-bottom: 1px solid $gray-800;
-      color: $white;
-      text-align: right;
-      text-transform: uppercase;
+    .pubkey {
+      max-width: 70px;
+      @include for-size(tablet-portrait-up) {
+        max-width: 100px;
+      }
+
+      @include for-size(big-desktop-up) {
+        max-width: 180px;
+      }
+
+      @media (min-width: 1300px) {
+        max-width: 280px;
+      }
+
+      @media (min-width: 1400px) {
+        max-width: 350px;
+      }
     }
 
-    td {
-      padding: 2px 5px;
-      border-top: 1px solid $gray-800;
-      color: $gray-400;
-      text-align: right;
-    }
-
-    td.fee {
+    .fee {
       min-width: 90px;
       color: $orange-600;
     }
 
-    .td-missed {
+    .missed {
       color: $orange-600;
     }
 
-    .pubkey {
-      color: $white;
-      font-size: 11px;
-      text-align: left;
-    }
-    .td-pubkey {
-      color: $gray-400;
-      font-size: 11px;
-      text-align: left;
-    }
-
-    .pubkey div {
-      overflow: hidden;
-      width: 100px;
-      text-overflow: ellipsis;
-      white-space: nowrap;
-    }
-
     .domain {
-      overflow: hidden;
       max-width: 140px;
-      color: $white;
-      text-align: left;
-      text-overflow: ellipsis;
-      white-space: nowrap;
-      @include medium;
-    }
-    .td-domain {
-      overflow: hidden;
-      max-width: 140px;
-      color: $gray-400;
-      text-align: left;
-      text-overflow: ellipsis;
-      white-space: nowrap;
       @include medium;
     }
 
@@ -82,13 +50,12 @@
       @include bold;
     }
 
-    .unl {
-      color: $white;
-      text-align: center;
+    .unl, .nUnl {
+      max-width: 40px;
+    }
 
-      img {
-        height: 16px;
-      }
+    .unl {
+      text-align: center;
     }
 
     .unl.yes {
@@ -96,7 +63,6 @@
     }
 
     .nUnl {
-      color: $white;
       text-align: center;
       text-transform: none;
 
@@ -110,23 +76,7 @@
       color: #c97;
     }
 
-    .pubkey,
-    .score.h1,
-    .score.d30,
-    .fee {
-      display: none;
-    }
-
     @include for-size(tablet-portrait-up) {
-      td {
-        padding: 15px 10px;
-        color: $gray-400;
-      }
-
-      .domain {
-        max-width: auto;
-      }
-
       .score.d30 {
         display: table-cell;
       }

src/containers/Tokens/TokensTable/index.js
@@ -61,7 +61,7 @@ const TokensTable = props => {
           {!tokenInfo.gravatar && currencySymbol && ' '}
           <Currency currency={tokenInfo.currency} />
         </td>
-        <td className="issuer">
+        <td className="issuer text-truncate">
           <Link className="token-issuer" title={tokenName} to={`/token/${tokenName}`}>
             {tokenInfo.domain ? tokenInfo.domain : tokenInfo.issuer}
           </Link>
@@ -92,7 +92,7 @@ const TokensTable = props => {
   return (
     <div className="tokens-table-container">
       <div className="tokens-table">
-        <table>
+        <table className="basic">
           <tbody>
             {!isLoading && (
               <tr className="tokens-table-header">

src/containers/Tokens/TokensTable/styles.scss
@@ -1,7 +1,5 @@
 @import '../../shared/css/variables.scss';
 .tokens-table-container {
-  display: flex;
-  width: 100%;
   margin-bottom: 40px;
   .loader {
     top: 1px;
@@ -20,35 +18,8 @@
   }
 
   .tokens-table {
-    position: relative;
-    display: flex;
-    width: 100%;
-    flex-flow: row wrap;
-
     table {
-      width: 100%;
-      border-top: none;
-      border-bottom: 1px solid $gray-800;
-      margin: auto;
-      border-collapse: collapse;
-      color: $gray-400;
-      font-size: 13px;
-      text-align: left;
-
-      a {
-        color: inherit;
-        text-decoration: none;
-      }
-
-      .tokens-table-header {
-        border-bottom: 1px solid $gray-800;
-      }
-
       th {
-        padding: 22px 10px;
-        color: $gray-400;
-        text-transform: uppercase;
-
         .q-tooltip:hover::after {
           font-weight: normal;
           text-transform: none;
@@ -76,13 +47,8 @@
         }
       }
 
-      .tokens-table-row {
-        border-top: 1px solid $gray-800;
-      }
-
-      td {
-        padding: 2px 5px;
-        color: $white;
+      .rank {
+        width: 55px;
       }
 
       img {

src/containers/Tokens/styles.scss
@@ -1,12 +1,6 @@
 @import '../shared/css/variables.scss';
 
 .token-discovery-page {
-  display: flex;
-  height: 100%;
-  min-height: $min-height;
-  flex-direction: column;
-  align-content: center;
-  justify-content: space-between;
   margin: 106px 10% 16px 10%;
 
   .loader {

src/containers/shared/css/table.scss
@@ -1,0 +1,56 @@
+@import '../../shared/css/variables.scss';
+
+table.basic {
+  overflow: hidden;
+  width: 100%;
+  border-top: none;
+  margin: auto;
+  border-collapse: collapse;
+  color: $white;
+  font-size: 12px;
+  letter-spacing: 0px;
+  text-align: left;
+
+  a {
+    color: inherit;
+    text-decoration: none;
+  }
+
+  tr {
+    border-bottom: 1px solid $gray-800;
+  }
+
+  td, th {
+    &.right {
+      text-align: right;
+    }
+
+    &.center {
+      text-align: center;
+    }
+
+    &.text-truncate {
+      overflow: hidden;
+      text-overflow: ellipsis;
+      white-space: nowrap;
+    }
+  }
+
+  th {
+    padding: 22px 5px;
+    color: $gray-400;
+    text-align: left;
+    text-transform: uppercase;
+  }
+
+  td {
+    padding: 15px 5px;
+
+    @include for-size(tablet-portrait-up) {
+      td {
+        padding: 15px 10px;
+        color: $gray-400;
+      }
+    }
+  }
+}

commit 513b0bfa2442f1e363b58685a5471eb227bc3212

fix(validators): Mark any valid agreement < 1 in red
I broke this in PR #165.

src/containers/Network/ValidatorsTable.jsx
@@ -64,7 +64,7 @@ class ValidatorsTable extends Component {
 
     return d ? (
       <td
-        className={`${className} score ${d.score < 1 ? 'td-missed' : ''}`}
+        className={`${className} score ${d.score < 1 ? 'missed' : ''}`}
         title={t('missed_validations', { count: d.missed })}
       >
         {Number.parseFloat(d.score).toFixed(5)}