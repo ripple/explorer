799a9c458fbb603578393b529e9b35e7a6a6e4b8
Mayukha Vadari <mvadari@ripple.com>
2022-06-30 18:22:40 -0400

    implement mapper

--- src/containers/shared/components/Transaction/SidechainXChainSeqNumCreate/parser.ts
+++ src/containers/shared/components/Transaction/SidechainXChainSeqNumCreate/parser.ts
@@ -1,10 +1,3 @@
-interface SignerEntry {
-  SignerEntry: {
-    Account: string;
-    SignerWeight: string;
-  };
-}
-
 export function parser(tx: any) {
   // TODO: update when Sidechain return bug is fixed
   return {
@@ -13,7 +6,5 @@ export function parser(tx: any) {
     sourceIssue: tx.Sidechain?.src_chain_issue || 'fakeXRP',
     destinationDoor: tx.Sidechain?.dst_chain_door || 'rFakeAccount2',
     destinationIssue: tx.Sidechain?.dst_chain_issue || 'fakeXRP2',
-    signerQuorum: tx.SignerQuorum,
-    signerEntries: tx.SignerEntries.map((entry: SignerEntry) => entry.SignerEntry),
   };
 }
