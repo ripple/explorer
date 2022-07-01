df8d223454d6ce9ecb5b1e327f3291a6f6df9b46
Mayukha Vadari <mvadari@ripple.com>
2022-07-01 10:25:39 -0400

    implement XChainTransfer

--- src/containers/shared/components/Transaction/index.ts
+++ src/containers/shared/components/Transaction/index.ts
@@ -1,10 +1,12 @@
 import { OfferCreateTransaction as OfferCreate } from './OfferCreate'
+import { SidechainXChainTransferTransaction as SidechainXChainTransfer } from './SidechainXChainTransfer'
 import { XChainCreateBridgeTransaction as XChainCreateBridge } from './XChainCreateBridge'
 import { XChainCreateClaimIDTransaction as XChainCreateClaimID } from './XChainCreateClaimID'
 import { TransactionMapping } from './types'
 
 export const transactionTypes: { [key: string]: TransactionMapping } = {
   OfferCreate,
+  SidechainXChainTransfer,
   XChainCreateBridge,
   XChainCreateClaimID,
 }
