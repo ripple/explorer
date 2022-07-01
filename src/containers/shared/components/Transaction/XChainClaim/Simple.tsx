840f6a0cf4d9d927d8ffa2b410fe311202cc166b
Mayukha Vadari <mvadari@ripple.com>
2022-07-01 10:42:08 -0400

    implement XChainClaim (ish)

---
+++ src/containers/shared/components/Transaction/XChainClaim/Simple.tsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import { useTranslation } from 'react-i18next'
+import Account from '../../Account'
+import { TransactionSimpleComponent, TransactionSimpleProps } from '../types'
+import { SimpleRow } from '../SimpleRow'
+
+export const Simple: TransactionSimpleComponent = (
+  props: TransactionSimpleProps,
+) => {
+  const { t } = useTranslation()
+  const { data } = props
+  const {
+    account,
+    sourceDoor,
+    sourceIssue,
+    destinationDoor,
+    destinationIssue,
+  } = data.instructions
+
+  return (
+    <>
+      <SimpleRow label={t('from_account')}>
+        <Account account={account} />
+      </SimpleRow>
+      <SimpleRow label={t('source_chain_door')}>
+        <Account account={sourceDoor} />
+      </SimpleRow>
+      <SimpleRow label={t('source_chain_issue')}>{sourceIssue}</SimpleRow>
+      <SimpleRow label={t('destination_chain_door')}>
+        <Account account={destinationDoor} />
+      </SimpleRow>
+      <SimpleRow label={t('destination_chain_issue')}>
+        {destinationIssue}
+      </SimpleRow>
+    </>
+  )
+}
--- src/containers/shared/components/Transaction/index.ts
+++ src/containers/shared/components/Transaction/index.ts
@@ -1,4 +1,5 @@
 import { OfferCreateTransaction as OfferCreate } from './OfferCreate'
+import { XChainClaimTransaction as XChainClaim } from './XChainClaim'
 import { XChainCommitTransaction as XChainCommit } from './XChainCommit'
 import { XChainCreateBridgeTransaction as XChainCreateBridge } from './XChainCreateBridge'
 import { XChainCreateClaimIDTransaction as XChainCreateClaimID } from './XChainCreateClaimID'
@@ -6,6 +7,7 @@ import { TransactionMapping } from './types'
 
 export const transactionTypes: { [key: string]: TransactionMapping } = {
   OfferCreate,
+  XChainClaim,
   XChainCommit,
   XChainCreateBridge,
   XChainCreateClaimID,
