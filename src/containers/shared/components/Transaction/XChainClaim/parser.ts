ded60825ed22deb3ca9b44591fb7b24f197db228
Mayukha Vadari <mvadari@ripple.com>
2022-07-01 10:42:08 -0400

    implement XChainClaim (ish)

---
+++ src/containers/shared/components/Transaction/XChainClaim/parser.ts
@@ -0,0 +1,8 @@
+export function parser(tx: any) {
+  return {
+    lockingDoor: tx.XChainBridge.LockingChainDoor,
+    lockingIssue: tx.XChainBridge.LockingChainIssue,
+    issuingDoor: tx.XChainBridge.IssuingChainDoor,
+    issuingIssue: tx.XChainBridge.IssuingChainIssue,
+  }
+}
