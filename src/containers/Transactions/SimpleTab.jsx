9793ecbe1e771ec02f8a786094bc024662601d8a
Mayukha Vadari <mvadari@ripple.com>
2022-07-14 10:56:24 +0100

    make CSS changes

--- src/containers/Transactions/SimpleTab.jsx
+++ src/containers/Transactions/SimpleTab.jsx
@@ -135,7 +135,7 @@ class SimpleTab extends Component {
     }
 
     return (
-      <div className="simple-body">
+      <div className="simple-body-tx">
         <div className="rows">
           <Simple
             t={t}
