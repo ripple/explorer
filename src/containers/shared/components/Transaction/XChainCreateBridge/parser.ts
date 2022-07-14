68aea70f999977901b27f84db4d93442a158524a
Mayukha Vadari <mvadari@ripple.com>
2022-07-14 10:04:02 +0100

    handle renames

---
+++ src/containers/shared/components/Transaction/XChainCreateBridge/parser.ts
@@ -0,0 +1,11 @@
+export function parser(tx: any) {
+  // TODO: update when Bridge return bug is fixed
+  return {
+    lockingDoor: tx.Bridge?.src_chain_door || 'rFakeAccount',
+    lockingIssue: tx.Bridge?.src_chain_issue || 'fakeXRP',
+    issuingDoor: tx.Bridge?.dst_chain_door || 'rFakeAccount2',
+    issuingIssue: tx.Bridge?.dst_chain_issue || 'fakeXRP2',
+    signatureReward: tx.SignatureReward,
+    minAccountCreateAmount: tx.MinAccountCreateAmount || 'N/A',
+  };
+}
--- src/containers/shared/components/Transaction/index.ts
+++ src/containers/shared/components/Transaction/index.ts
@@ -1,8 +1,8 @@
 import { OfferCreateTransaction as OfferCreate } from './OfferCreate';
-import { SidechainCreateTransaction as SidechainCreate } from './SidechainCreate';
+import { XChainCreateBridgeTransaction as XChainCreateBridge } from './XChainCreateBridge';
 import { TransactionMapping } from './types';
 
 export const transactionTypes: { [key: string]: TransactionMapping } = {
   OfferCreate,
-  SidechainCreate,
+  XChainCreateBridge,
 };
