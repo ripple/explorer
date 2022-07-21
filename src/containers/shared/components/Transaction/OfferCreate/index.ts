5e0eede6b70b44bbb2e1c871afd4ef411a86c8b6
Caleb Kniffen <ckniffen@ripple.com>
2022-07-21 17:30:16 -0500

    Simplify registering a transaction type

--- src/containers/shared/components/Transaction/OfferCreate/index.ts
+++ src/containers/shared/components/Transaction/OfferCreate/index.ts
@@ -1,13 +1,4 @@
-import { TransactionMapping } from '../types';
-
-import { OfferCreateSimple } from './OfferCreateSimple';
-import { OfferCreateDescription } from './OfferCreateDescription';
-import { offerCreateMapper } from './offerCreateMapper';
-import { OfferCreateTableDetail } from './OfferCreateTableDetail';
-
-export const OfferCreateTransaction: TransactionMapping = {
-  Description: OfferCreateDescription,
-  Simple: OfferCreateSimple,
-  TableDetail: OfferCreateTableDetail,
-  mapper: offerCreateMapper,
-};
+export { OfferCreateSimple as Simple } from './OfferCreateSimple';
+export { OfferCreateDescription as Description } from './OfferCreateDescription';
+export { offerCreateMapper as mapper } from './offerCreateMapper';
+export { OfferCreateTableDetail as TableDetail } from './OfferCreateTableDetail';
--- src/containers/shared/components/Transaction/index.ts
+++ src/containers/shared/components/Transaction/index.ts
@@ -1,4 +1,4 @@
-import { OfferCreateTransaction as OfferCreate } from './OfferCreate';
+import * as OfferCreate from './OfferCreate';
 import { TransactionMapping } from './types';
 
 export const transactionTypes: { [key: string]: TransactionMapping } = {
