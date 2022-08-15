41b8376da7378dc1f39d5c18cc0261887b6ff594
Mayukha Vadari <mvadari@ripple.com>
2022-08-15 20:19:42 +0200

    feat: add support for `XChainCreateBridge` tx (#184)
    * set up basics
    
    * implement initial SidechainCreate stuff
    
    * create SimpleRow helper class
    
    * update translations
    
    * handle renames
    
    * update to new XChainCreateBridge
    
    * fix linting issues
    
    * remove ws use
    
    * move translation
    
    * respond to comments
    
    * respond to comments
    
    * use Amount component
    
    * clean up null operators
    
    * fix issues
    
    * only link the address on this chain
    
    * add helper type for bridge stuff
    
    * respond to comments
    
    * add test
    
    * use XRP_BASE

---
+++ src/containers/shared/components/Transaction/XChainCreateBridge/Simple.tsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import { useTranslation } from 'react-i18next'
+import { Amount } from '../../Amount'
+import { TransactionSimpleComponent, TransactionSimpleProps } from '../types'
+import { SimpleRow } from '../SimpleRow'
+import { XChainBridge } from '../XChainBridge'
+
+export const Simple: TransactionSimpleComponent = (
+  props: TransactionSimpleProps,
+) => {
+  const { t } = useTranslation()
+  const {
+    data: {
+      instructions: {
+        lockingDoor,
+        lockingIssue,
+        issuingDoor,
+        issuingIssue,
+        signatureReward,
+        minAccountCreateAmount,
+        bridgeOwner,
+      },
+    },
+  } = props
+
+  return (
+    <>
+      <XChainBridge
+        lockingDoor={lockingDoor}
+        lockingIssue={lockingIssue}
+        issuingDoor={issuingDoor}
+        issuingIssue={issuingIssue}
+        signatureReward={signatureReward}
+        bridgeOwner={bridgeOwner}
+      />
+      {minAccountCreateAmount && (
+        <SimpleRow label={t('min_account_create_amount')}>
+          <Amount value={minAccountCreateAmount} />
+        </SimpleRow>
+      )}
+    </>
+  )
+}
