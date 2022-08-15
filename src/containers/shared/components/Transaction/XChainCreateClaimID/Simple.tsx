9e34bd574df670f9fb2772f3d74d8f42c31a02c1
Mayukha Vadari <mvadari@ripple.com>
2022-08-15 20:26:19 +0200

    feat: add support for `XChainCreateClaimID` tx (#185)
    * set up outline
    
    * implement mapper
    
    * handle all renames
    
    * add other tx stuff
    
    * update to new rippled
    
    * fix problems
    
    * only link the address on the chain
    
    * use XChainBridge obj

---
+++ src/containers/shared/components/Transaction/XChainCreateClaimID/Simple.tsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import { useTranslation } from 'react-i18next'
+import Account from '../../Account'
+import { TransactionSimpleComponent, TransactionSimpleProps } from '../types'
+import { SimpleRow } from '../SimpleRow'
+import { XChainBridge } from '../XChainBridge'
+
+export const Simple: TransactionSimpleComponent = (
+  props: TransactionSimpleProps,
+) => {
+  const { t } = useTranslation()
+  const {
+    data: {
+      instructions: {
+        lockingDoor,
+        lockingIssue,
+        issuingDoor,
+        issuingIssue,
+        signatureReward,
+        otherChainAccount,
+        bridgeOwner,
+      },
+    },
+  } = props
+
+  return (
+    <>
+      <XChainBridge
+        lockingDoor={lockingDoor}
+        lockingIssue={lockingIssue}
+        issuingDoor={issuingDoor}
+        issuingIssue={issuingIssue}
+        signatureReward={signatureReward}
+        bridgeOwner={bridgeOwner}
+      />
+      <SimpleRow label={t('other_chain_account')}>
+        <Account account={otherChainAccount} link={false} />
+      </SimpleRow>
+    </>
+  )
+}
