3afaf5f02145035bcd19226b1e2074170c0af7ef
Mayukha Vadari <mvadari@ripple.com>
2022-08-15 15:46:55 +0200

    add helper type for bridge stuff

--- src/containers/shared/components/Transaction/XChainCreateBridge/Simple.tsx
+++ src/containers/shared/components/Transaction/XChainCreateBridge/Simple.tsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import { useTranslation } from 'react-i18next'
-import Account from '../../Account'
 import { Amount } from '../../Amount'
 import { TransactionSimpleComponent, TransactionSimpleProps } from '../types'
 import { SimpleRow } from '../SimpleRow'
+import { XChainBridge } from '../XChainBridge'
 
 export const Simple: TransactionSimpleComponent = (
   props: TransactionSimpleProps,
@@ -25,17 +25,14 @@ export const Simple: TransactionSimpleComponent = (
 
   return (
     <>
-      <SimpleRow label={t('locking_chain_door')}>
-        <Account account={lockingDoor} link={lockingDoor === bridgeOwner} />
-      </SimpleRow>
-      <SimpleRow label={t('locking_chain_issue')}>{lockingIssue}</SimpleRow>
-      <SimpleRow label={t('issuing_chain_door')}>
-        <Account account={issuingDoor} link={issuingDoor === bridgeOwner} />
-      </SimpleRow>
-      <SimpleRow label={t('issuing_chain_issue')}>{issuingIssue}</SimpleRow>
-      <SimpleRow label={t('signature_reward')}>
-        <Amount value={signatureReward} />
-      </SimpleRow>
+      <XChainBridge
+        lockingDoor={lockingDoor}
+        lockingIssue={lockingIssue}
+        issuingDoor={issuingDoor}
+        issuingIssue={issuingIssue}
+        signatureReward={signatureReward}
+        bridgeOwner={bridgeOwner}
+      />
       {minAccountCreateAmount && (
         <SimpleRow label={t('min_account_create_amount')}>
           <Amount value={minAccountCreateAmount} />
