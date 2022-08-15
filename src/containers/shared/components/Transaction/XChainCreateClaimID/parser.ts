b571a7da67ecf4d5023d0a23b0e5751d04cdc90e
Mayukha Vadari <mvadari@ripple.com>
2022-08-15 15:12:13 +0200

    only link the address on the chain

--- src/containers/shared/components/Transaction/XChainCreateClaimID/parser.ts
+++ src/containers/shared/components/Transaction/XChainCreateClaimID/parser.ts
@@ -1,4 +1,9 @@
-export function parser(tx: any) {
+export function parser(tx: any, meta: any) {
+  const affectedNodes = meta.AffectedNodes
+  const bridgeMeta = affectedNodes.filter(
+    (node: any) =>
+      node.ModifiedNode && node.ModifiedNode.LedgerEntryType === 'Bridge',
+  )[0]
   return {
     lockingDoor: tx.XChainBridge.LockingChainDoor,
     lockingIssue: tx.XChainBridge.LockingChainIssue,
@@ -6,5 +11,6 @@ export function parser(tx: any) {
     issuingIssue: tx.XChainBridge.IssuingChainIssue,
     signatureReward: tx.SignatureReward,
     otherChainAccount: tx.OtherChainAccount,
+    bridgeOwner: bridgeMeta.ModifiedNode.FinalFields.Account,
   }
 }
