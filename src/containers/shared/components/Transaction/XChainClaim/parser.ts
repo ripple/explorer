5cd78445303a70a38e2d2ac16cf83d3444348cfc
Mayukha Vadari <mvadari@ripple.com>
2022-08-18 09:45:22 +0200

    add the rest of the info

--- src/containers/shared/components/Transaction/XChainClaim/parser.ts
+++ src/containers/shared/components/Transaction/XChainClaim/parser.ts
@@ -1,9 +1,22 @@
-export function parser(tx: any) {
-  console.log(tx)
+export function parser(tx: any, meta: any) {
+  const affectedNodes = meta.AffectedNodes
+  const modifiedAccountRoots = affectedNodes.filter(
+    (node: any) =>
+      node.ModifiedNode && node.ModifiedNode.LedgerEntryType === 'AccountRoot',
+  )
+  const doorNode = modifiedAccountRoots.filter(
+    (node: any) =>
+      node.ModifiedNode.FinalFields.Balance <
+      node.ModifiedNode.PreviousFields.Balance,
+  )[0]
   return {
     lockingDoor: tx.XChainBridge.LockingChainDoor,
     lockingIssue: tx.XChainBridge.LockingChainIssue,
     issuingDoor: tx.XChainBridge.IssuingChainDoor,
     issuingIssue: tx.XChainBridge.IssuingChainIssue,
+    bridgeOwner: doorNode.ModifiedNode.FinalFields.Account,
+    claimId: tx.XChainClaimID,
+    destination: tx.Destination,
+    amount: tx.Amount,
   }
 }
