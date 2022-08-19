67de493add8e733b868533e41194cb0ed43fd3d6
Caleb Kniffen <ckniffen@ripple.com>
2022-08-19 18:14:38 -0500

    refactor(SetRegularKey): Update transaction to use new scheme
    Part of #205 and is related to #179

--- src/containers/shared/components/Transaction/index.ts
+++ src/containers/shared/components/Transaction/index.ts
@@ -3,9 +3,11 @@ import { XChainCommitTransaction as XChainCommit } from './XChainCommit'
 import { XChainCreateBridgeTransaction as XChainCreateBridge } from './XChainCreateBridge'
 import { XChainCreateClaimIDTransaction as XChainCreateClaimID } from './XChainCreateClaimID'
 import { TransactionMapping } from './types'
+import { SetRegularKeyTransaction as SetRegularKey } from './SetRegularKey'
 
 export const transactionTypes: { [key: string]: TransactionMapping } = {
   OfferCreate,
+  SetRegularKey,
   XChainCommit,
   XChainCreateBridge,
   XChainCreateClaimID,
