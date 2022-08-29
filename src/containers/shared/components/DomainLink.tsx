bbfcedfafe703b06385e3508d53e8607992e5be5
Connor Chen <connorjchen2784@gmail.com>
2022-08-29 12:49:50 -0400

    implemented domainlink component

---
+++ src/containers/shared/components/DomainLink.tsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { decodeHex } from '../transactionUtils'
+
+interface Props {
+  className?: string
+  decode?: boolean
+  domain: string
+}
+
+const DomainLink = (props: Props) => {
+  const { className, decode, domain } = props
+
+  return (
+    <a
+      className={className}
+      rel="noopener noreferrer"
+      target="_blank"
+      href={`http://${decode ? decodeHex(domain) : domain}`}
+    >
+      {decode ? decodeHex(domain) : domain}
+    </a>
+  )
+}
+
+DomainLink.defaultProps = {
+  className: '',
+  decode: false,
+}
+
+DomainLink.propTypes = {
+  className: PropTypes.string,
+  decode: PropTypes.bool,
+  domain: PropTypes.string.isRequired,
+}
+
+export default DomainLink
