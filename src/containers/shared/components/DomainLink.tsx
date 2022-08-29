c5e5be3b512c7d3e623868587dfcb9924425c773
Connor Chen <connorjchen2784@gmail.com>
2022-08-29 14:21:33 -0400

    replaced http for https, refactored DomainLink

--- src/containers/shared/components/DomainLink.tsx
+++ src/containers/shared/components/DomainLink.tsx
@@ -1,37 +1,25 @@
 import React from 'react'
-import PropTypes from 'prop-types'
 import { decodeHex } from '../transactionUtils'
 
-interface Props {
+export interface Props {
   className?: string
   decode?: boolean
   domain: string
 }
 
 const DomainLink = (props: Props) => {
-  const { className, decode, domain } = props
+  const { className = '', decode = false, domain } = props
 
   return (
     <a
       className={className}
       rel="noopener noreferrer"
       target="_blank"
-      href={`http${decode ? '' : 's'}://${decode ? decodeHex(domain) : domain}`}
+      href={`https://${decode ? decodeHex(domain) : domain}`}
     >
       {decode ? decodeHex(domain) : domain}
     </a>
   )
-}
-
-DomainLink.defaultProps = {
-  className: '',
-  decode: false,
-}
-
-DomainLink.propTypes = {
-  className: PropTypes.string,
-  decode: PropTypes.bool,
-  domain: PropTypes.string.isRequired,
 }
 
 export default DomainLink
