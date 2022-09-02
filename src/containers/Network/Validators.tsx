94622259b6aad262bab014ce733e342bb2b98456
Phu Pham <ppham@ripple.com>
2022-09-02 12:12:47 -0400

    Add barchart using recharts and refractor code

--- src/containers/Network/Validators.tsx
+++ src/containers/Network/Validators.tsx
@@ -1,12 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 import { useTranslation } from 'react-i18next'
-<<<<<<< HEAD
 import NetworkTabs from './NetworkTab'
-=======
-import { useRouteMatch } from 'react-router'
-import Tabs from '../shared/components/Tabs'
->>>>>>> 1488579 (refractor validators)
 import Streams from '../shared/components/Streams'
 import ValidatorsTable from './ValidatorsTable'
 import Log from '../shared/log'
@@ -14,7 +9,6 @@ import { localizeNumber } from '../shared/utils'
 import { useLanguage } from '../shared/hooks'
 import Hexagons from './Hexagons'
 
-<<<<<<< HEAD
 export const Validators = () => {
   const language = useLanguage()
   const { t } = useTranslation()
@@ -22,43 +16,6 @@ export const Validators = () => {
   const [validations, setValidations] = useState([])
   const [metrics, setMetrics] = useState({})
   const [unlCount, setUnlCount] = useState(0)
-
-=======
-const mergeLatest = (validators: any[] = [], live: any[] = []) => {
-  const latest: any = {}
-  live.forEach((d) => {
-    latest[d.pubkey] = {
-      ledger_index: d.ledger_index,
-      ledger_hash: d.ledger_hash,
-    }
-  })
-
-  return validators.map((d) => {
-    const updated: any = {}
-    if (
-      latest[d.signing_key] &&
-      latest[d.signing_key].ledger_index > d.ledger_index
-    ) {
-      updated.ledger_index = latest[d.signing_key].ledger_index
-      updated.ledger_hash = latest[d.signing_key].ledger_hash
-    }
-
-    return Object.assign(d, updated)
-  })
-}
-export const Validators = () => {
-  const language = useLanguage()
-  const { t } = useTranslation()
-
-  const [vList, setVList] = useState({})
-  const [liveValidators, setLiveValidators] = useState([])
-  const [metrics, setMetrics] = useState({})
-  const [validators, setValidators] = useState([])
-  const [unlCount, setUnlCount] = useState(0)
-  const { path = '/' } = useRouteMatch()
-
-  liveValidators.slice(0, 1)
->>>>>>> 1488579 (refractor validators)
   useEffect(() => {
     fetchData()
     const interval = setInterval(fetchData, 5000)
@@ -73,36 +30,17 @@ export const Validators = () => {
     axios
       .get(url)
       .then((resp) => {
-<<<<<<< HEAD
         const newValidatorList: any = {}
         resp.data.forEach((v: any) => {
           newValidatorList[v.signing_key] = v
         })
 
         setVList(() => newValidatorList)
-=======
-        const newVList: any = {}
-        resp.data.forEach((v: any) => {
-          newVList[v.signing_key] = {
-            signing_key: v.signing_key,
-            master_key: v.master_key,
-            unl: v.unl,
-            domain: v.domain,
-          }
-        })
-
-        setVList(newVList)
-        // @ts-ignore
-        setValidators((prevValidators: any[]) =>
-          mergeLatest(resp.data, prevValidators),
-        )
->>>>>>> 1488579 (refractor validators)
         setUnlCount(resp.data.filter((d: any) => Boolean(d.unl)).length)
       })
       .catch((e) => Log.error(e))
   }
 
-<<<<<<< HEAD
   const updateValidators = (newValidations: any[]) => {
     // @ts-ignore
     setValidations(newValidations)
@@ -120,20 +58,6 @@ export const Validators = () => {
   }
 
   const validatorCount = Object.keys(vList).length
-=======
-  const updateValidators = (newValidators: any[]) => {
-    // @ts-ignore
-    setLiveValidators(newValidators)
-    // @ts-ignore
-    setValidators((prevValidators: any[]) =>
-      mergeLatest(prevValidators, newValidators),
-    )
-  }
-
-  const tabs = ['nodes', 'validators', 'chart']
-  // @ts-ignore
-
->>>>>>> 1488579 (refractor validators)
   return (
     <div className="network-page">
       <Streams
@@ -141,7 +65,6 @@ export const Validators = () => {
         updateValidators={updateValidators}
         updateMetrics={setMetrics}
       />
-<<<<<<< HEAD
       {validatorCount && (
         // @ts-ignore
         <Hexagons data={validations} list={vList} />
@@ -152,18 +75,6 @@ export const Validators = () => {
             <span>{t('validators_found')}: </span>
             <span>
               {localizeNumber(validatorCount, language)}
-=======
-      {
-        // @ts-ignore
-        validators.length && <Hexagons data={liveValidators} list={vList} />
-      }
-      <div className="stat">
-        {validators && (
-          <>
-            <span>{t('validators_found')}: </span>
-            <span>
-              {localizeNumber(validators.length, language)}
->>>>>>> 1488579 (refractor validators)
               {unlCount !== 0 && (
                 <i>
                   {' '}
@@ -175,13 +86,8 @@ export const Validators = () => {
         )}
       </div>
       <div className="wrap">
-<<<<<<< HEAD
         <NetworkTabs selected="validators" />
         <ValidatorsTable validators={vList} metrics={metrics} />
-=======
-        <Tabs tabs={tabs} selected="validators" path={path} />
-        <ValidatorsTable validators={validators} metrics={metrics} />
->>>>>>> 1488579 (refractor validators)
       </div>
     </div>
   )
--- src/containers/shared/components/BarChart.tsx
+++ src/containers/shared/components/BarChart.tsx
@@ -1,107 +1,99 @@
-import * as d3 from 'd3'
-import React, { useEffect, useRef } from 'react'
+import React, { useState } from 'react'
+import { useTranslation } from 'react-i18next'
+import {
+  BarChart,
+  Bar,
+  XAxis,
+  YAxis,
+  Tooltip,
+  TooltipProps,
+  Label,
+} from 'recharts'
+import {
+  ValueType,
+  NameType,
+} from 'recharts/src/component/DefaultTooltipContent'
 import '../css/barchart.css'
 
-interface Data {
-  label: string
-  value: number
+interface Props {
+  data: any[]
 }
 
-interface BarChartProps {
-  data: Data[]
+interface BarCoordinates {
+  x: number
+  y: number
 }
 
-interface AxisBottomProps {
-  scale: d3.ScaleBand<string>
-  transform: string
+const CustomTooltip = ({
+  active,
+  payload,
+  label,
+}: TooltipProps<ValueType, NameType>) => {
+  const { t } = useTranslation()
+  if (active) {
+    return (
+      <div className="custom-tooltip">
+        <p className="label">Version: {label}</p>
+        <p className="value"># of Nodes: {payload?.[0].payload.count}</p>
+      </div>
+    )
+  }
+  return null
 }
 
-function AxisBottom({ scale, transform }: AxisBottomProps) {
-  const ref = useRef<SVGGElement>(null)
-
-  useEffect(() => {
-    if (ref.current) {
-      d3.select(ref.current).attr('class', 'yAxis').call(d3.axisBottom(scale))
-    }
-  }, [scale])
-
-  return <g ref={ref} transform={transform} />
-}
-
-interface AxisLeftProps {
-  scale: d3.ScaleLinear<number, number, never>
-}
-
-function AxisLeft({ scale }: AxisLeftProps) {
-  const ref = useRef<SVGGElement>(null)
-
-  useEffect(() => {
-    if (ref.current) {
-      d3.select(ref.current)
-        .attr('class', 'xAxis')
-        .call(d3.axisLeft(scale))
-        .append('text')
-        .attr('text-anchor', 'middle')
-        .attr('transform', 'rotate(-90)')
-        .attr('padding-right', '70px')
-        .text('Y Axis Label')
-    }
-  }, [scale])
-
-  return <g ref={ref} />
-}
-
-interface BarsProps {
-  data: BarChartProps['data']
-  height: number
-  scaleX: AxisBottomProps['scale']
-  scaleY: AxisLeftProps['scale']
-}
-
-function Bars({ data, height, scaleX, scaleY }: BarsProps) {
+const BarChartRenderer = (props: Props) => {
+  const { data } = props
+  const { t } = useTranslation()
+  const [posData, setposData] = useState<BarCoordinates>({ x: 0, y: 0 })
   return (
-    <>
-      {data.map(({ value, label }) => (
-        <rect
-          key={`bar-${label}`}
-          x={scaleX(label)}
-          y={scaleY(value)}
-          width={scaleX.bandwidth()}
-          height={height - scaleY(value)}
-          fill="teal"
+    <div className="barchart">
+      <BarChart
+        data={data}
+        width={1075}
+        height={532}
+        margin={{ top: 5, right: 20, bottom: 5, left: 0 }}
+      >
+        <XAxis
+          dataKey="label"
+          angle={-65}
+          dy={30}
+          height={90}
+          tickLine={false}
+          stroke="#9BA2B0"
         />
-      ))}
-    </>
+        <YAxis
+          className="yAxis"
+          tickLine={false}
+          tickFormatter={(tick) => `${tick}%`}
+          stroke="#9BA2B0"
+        >
+          <Label
+            className="yLabel"
+            value={t('% of total nodes')}
+            angle={-90}
+            position="insideTop"
+            dx={55}
+            dy={65}
+          />
+        </YAxis>
+        <Bar
+          dataKey="value"
+          barSize={30}
+          fill="#8884d8"
+          radius={[4, 4, 0, 0]}
+          onMouseOver={(dataY) => {
+            setposData({ x: dataY.x, y: dataY.y })
+          }}
+        />
+        <Tooltip
+          content={<CustomTooltip />}
+          cursor={false}
+          position={{ x: posData.x - 45, y: posData.y - 90 }}
+          offset={-10}
+        />
+      </BarChart>
+    </div>
   )
 }
 
-function BarChart({ data }: BarChartProps) {
-  const margin = { top: 10, right: 0, bottom: 20, left: 30 }
-  const width = 1075 - margin.left - margin.right
-  const height = 532 - margin.top - margin.bottom
-
-  const scaleX = d3
-    .scaleBand()
-    .domain(data.map(({ label }) => label))
-    .range([0, width])
-    .padding(0.5)
-  const scaleY = d3
-    .scaleLinear()
-    .domain([0, Math.max(...data.map(({ value }) => value))])
-    .range([height, 0])
-
-  return (
-    <svg
-      width={width + margin.left + margin.right}
-      height={height + margin.top + margin.bottom}
-    >
-      <g transform={`translate(${margin.left}, ${margin.top})`}>
-        <AxisBottom scale={scaleX} transform={`translate(0, ${height})`} />
-        <AxisLeft scale={scaleY} />
-        <Bars data={data} height={height} scaleX={scaleX} scaleY={scaleY} />
-      </g>
-    </svg>
-  )
-}
-
-export default BarChart
+export default BarChartRenderer
