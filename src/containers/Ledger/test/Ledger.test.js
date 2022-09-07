8daeb32885de2b4829808880a215b3923271fcf0
TusharPardhe <31487192+TusharPardhe@users.noreply.github.com>
2022-09-07 13:54:01 +0530

    "ledger-col" test case value changed

--- src/containers/Ledger/test/Ledger.test.js
+++ src/containers/Ledger/test/Ledger.test.js
@@ -64,7 +64,7 @@ describe('Ledger container', () => {
 
     expect(summary.length).toBe(1)
     expect(summary.find('.ledger-cols').length).toBe(1)
-    expect(summary.find('.ledger-col').length).toBeGreaterThanOrEqual(6)
+    expect(summary.find('.ledger-col').length).toBeGreaterThanOrEqual(4)
     expect(summary.find('.ledger-index').length).toBe(1)
     expect(summary.find('.closed-date').length).toBe(1)
     expect(summary.find('.ledger-hash').length).toBe(1)
