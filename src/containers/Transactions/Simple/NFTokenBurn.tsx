de3fabe7f01c5fbd5c984fa1f6fba1bed1b8de2a
Connor Chen <connorjchen2784@gmail.com>
2022-09-08 12:29:01 -0400

    Refactor NFTokenBurn

---
+++ src/containers/Transactions/Simple/NFTokenBurn.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import { SimpleRow } from '../../shared/components/Transaction/SimpleRow'
+
+export interface Props {
+  data: { instructions: { tokenID: string } }
+}
+
+const NFTokenBurn = (props: Props) => {
+  const {
+    data: {
+      instructions: { tokenID },
+    },
+  } = props
+
+  return (
+    <SimpleRow label="Token ID">
+      <div className="dt" data-test="token-id">
+        {tokenID}
+      </div>
+    </SimpleRow>
+  )
+}
+
+export default NFTokenBurn
