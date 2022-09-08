f213aa30e3af755a6ee44ff7e650dd19561f4aad
Connor Chen <connorjchen2784@gmail.com>
2022-09-08 14:33:58 -0400

    Refactored NFTokenCreateOffer

---
+++ src/containers/Transactions/Simple/NFTokenMint.tsx
@@ -0,0 +1,62 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+export interface Props {
+  data: {
+    instructions: {
+      account: string
+      amount: { currency: string; amount: number; issuer: string }
+      tokenID: string
+      isSellOffer: boolean
+      owner: string
+      offerID: string
+    }
+  }
+}
+
+const NFTokenMint = (props) => {
+  const {
+    data: {
+      instructions: { tokenID, tokenTaxon, uri },
+    },
+  } = props
+
+  return (
+    <>
+      <div className="row">
+        <div className="label">Token ID</div>
+        <div className="value">
+          <div className="dt" data-test="token-id">
+            {tokenID}
+          </div>
+        </div>
+      </div>
+      <div className="row">
+        <div className="label">Token Taxon</div>
+        <div className="value" data-test="token-taxon">
+          {tokenTaxon}
+        </div>
+      </div>
+      <div className="row">
+        <div className="label">URI</div>
+        <div className="value">
+          <div className="dt" data-test="token-uri">
+            {uri}
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+NFTokenMint.propTypes = {
+  data: PropTypes.shape({
+    instructions: PropTypes.shape({
+      tokenID: PropTypes.string,
+      tokenTaxon: PropTypes.number,
+      uri: PropTypes.string,
+    }),
+  }).isRequired,
+}
+
+export default NFTokenMint
