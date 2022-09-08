c0e8d979d8adc971e66dc6a089277ffe51397f30
Phu Pham <ppham@ripple.com>
2022-09-08 10:34:13 -0400

    jest test fixed

--- src/containers/Network/test/validators.test.js
+++ src/containers/Network/test/validators.test.js
@@ -69,7 +69,9 @@ describe('Validators Tab container', () => {
     })
 
     expect(wrapper.find('.validators').length).toBe(1)
-    expect(wrapper.find('.stat').html()).toBe('<div class="stat"></div>')
+    expect(wrapper.find('.stat').html()).toBe(
+      '<div class="stat"><span>validators_found: </span><span>1</span></div>',
+    )
     expect(wrapper.find('.validators-table').length).toBe(1)
 
     server.send(validationMessage)
