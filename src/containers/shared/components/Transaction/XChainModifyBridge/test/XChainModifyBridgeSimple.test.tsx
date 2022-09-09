746de277c64d86aa773cdbfd0b1eb4ea67eb799d
Mayukha Vadari <mvadari@ripple.com>
2022-09-09 13:27:21 -0700

    add MinAccountCreateAmount test

--- src/containers/shared/components/Transaction/XChainModifyBridge/test/XChainModifyBridgeSimple.test.tsx
+++ src/containers/shared/components/Transaction/XChainModifyBridge/test/XChainModifyBridgeSimple.test.tsx
@@ -4,6 +4,7 @@ import { BrowserRouter } from 'react-router-dom'
 import { mount, ReactWrapper } from 'enzyme'
 import { Simple } from '../Simple'
 import mockXChainModifyBridge from './mock_data/XChainModifyBridge.json'
+import mockXChainModifyBridgeMinAccountCreateAmount from './mock_data/XChainModifyBridgeMinAccountCreateAmount.json'
 import summarizeTransaction from '../../../../../../rippled/lib/txSummary'
 import i18n from '../../../../../../i18nTestConfig'
 
@@ -47,5 +48,27 @@ describe('XChainModifyBridgeSimple', () => {
     expectText(wrapper, 'issuing-chain-issue', 'XRP')
 
     expectText(wrapper, 'signature-reward', '\uE9000.01 XRP')
+  })
+
+  it('renders MinAccountCreateAmount', () => {
+    const wrapper = createWrapper(mockXChainModifyBridgeMinAccountCreateAmount)
+
+    // check XChainBridge parts
+    expectText(
+      wrapper,
+      'locking-chain-door',
+      'rnBnyot2gCJywLxLzfHQX2dUJqZ6oghUFp',
+    )
+    expect(wrapper.find(`[data-test="locking-chain-door"] a`)).toExist()
+    expectText(wrapper, 'locking-chain-issue', 'XRP')
+    expectText(
+      wrapper,
+      'issuing-chain-door',
+      'rHb9CJAWyB4rj91VRWn96DkukG4bwdtyTh',
+    )
+    expect(wrapper.find(`[data-test="issuing-chain-door"] a`)).not.toExist()
+    expectText(wrapper, 'issuing-chain-issue', 'XRP')
+
+    expectText(wrapper, 'min-account-create-amount', '\uE900100.00 XRP')
   })
 })
