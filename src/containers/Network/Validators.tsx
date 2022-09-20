05113ca719a102a077560cc489dc43f5ab0c560f
pdp2121 <71317875+pdp2121@users.noreply.github.com>
2022-09-20 13:44:21 -0400

    feat: Add bar chart version validators (#299)
    * version added
    
    * aggregation worked, chart initialized
    
    * refractor validators
    
    * custom barchart used with some formatting
    
    * aggregation worked, chart initialized
    
    * refractor validators
    
    * custom barchart used with some formatting
    
    * Add barchart using recharts and refractor code
    
    * refractor more
    
    * minor changes
    
    * remove old Validators
    
    * add scss
    
    * change link
    
    * add package-lock
    
    * remove scrollbar
    
    * refractor more
    
    * minor change
    
    * jest test fixed
    
    * remove redundant files
    
    * small fixes
    
    * adjust for small screen
    
    * aggregation worked, chart initialized
    
    * refractor validators
    
    * custom barchart used with some formatting
    
    * Add barchart using recharts and refractor code
    
    * refractor more
    
    * aggregation worked, chart initialized
    
    * custom barchart used with some formatting
    
    * minor changes
    
    * remove old Validators
    
    * add scss
    
    * change link
    
    * add package-lock
    
    * remove scrollbar
    
    * refractor more
    
    * minor change
    
    * jest test fixed
    
    * remove redundant files
    
    * small fixes
    
    * adjust for small screen
    
    * fix minor merge issues
    
    * delete yarn.lock
    
    * aggregation worked, chart initialized
    
    * refractor validators
    
    * custom barchart used with some formatting
    
    * Add barchart using recharts and refractor code
    
    * refractor more
    
    * aggregation worked, chart initialized
    
    * custom barchart used with some formatting
    
    * minor changes
    
    * remove old Validators
    
    * add scss
    
    * change link
    
    * add package-lock
    
    * remove scrollbar
    
    * refractor more
    
    * minor change
    
    * jest test fixed
    
    * remove redundant files
    
    * small fixes
    
    * adjust for small screen
    
    * aggregation worked, chart initialized
    
    * refractor validators
    
    * custom barchart used with some formatting
    
    * Add barchart using recharts and refractor code
    
    * refractor more
    
    * aggregation worked, chart initialized
    
    * refractor validators
    
    * custom barchart used with some formatting
    
    * minor changes
    
    * remove old Validators
    
    * add package-lock
    
    * refractor more
    
    * jest test fixed
    
    * remove redundant files
    
    * small fixes
    
    * delete yarn.lock
    
    * fix eslint
    
    * fix eslint more
    
    * rename files
    
    * remove unneccessary types
    
    * change file names
    
    * minor change
    
    * fix tooltip off canvas
    
    * clean up code
    
    * fix variable names
    
    * update more variable name
    
    * Refractor magic numbers and add ts-ignore description
    
    * fix tooltip
    
    * fix translation to use underscore
    
    * tooltip shown only when hover on bar region
    
    * more fine-tuned changes
    
    * small fixes
    
    * tooltip move with mouse hover
    
    * Fix longhand eslint
    
    * simplify fetching with usequery
    
    * small fixes
    
    Co-authored-by: Caleb Kniffen <ckniffen@ripple.com>

---
+++ src/containers/Network/Validators.tsx
@@ -0,0 +1,93 @@
+import React, { useState } from 'react'
+import axios from 'axios'
+import { useTranslation } from 'react-i18next'
+import { useQuery } from 'react-query'
+import NetworkTabs from './NetworkTabs'
+import Streams from '../shared/components/Streams'
+import ValidatorsTable from './ValidatorsTable'
+import Log from '../shared/log'
+import { localizeNumber, FETCH_INTERVAL_MILLIS } from '../shared/utils'
+import { useLanguage } from '../shared/hooks'
+import Hexagons from './Hexagons'
+
+export const Validators = () => {
+  const language = useLanguage()
+  const { t } = useTranslation()
+  const [vList, setVList] = useState<any>([{}])
+  const [validations, setValidations] = useState([])
+  const [metrics, setMetrics] = useState({})
+  const [unlCount, setUnlCount] = useState(0)
+
+  useQuery(['fetchValidatorsData'], () => fetchData(), {
+    refetchInterval: FETCH_INTERVAL_MILLIS,
+  })
+
+  const fetchData = () => {
+    const url = '/api/v1/validators?verbose=true'
+
+    axios
+      .get(url)
+      .then((resp) => {
+        const newValidatorList: any = {}
+        resp.data.forEach((v: any) => {
+          newValidatorList[v.signing_key] = v
+        })
+
+        setVList(() => newValidatorList)
+        setUnlCount(resp.data.filter((d: any) => Boolean(d.unl)).length)
+      })
+      .catch((e) => Log.error(e))
+  }
+
+  const updateValidators = (newValidations: any[]) => {
+    // @ts-ignore - Work around type assignment for complex validation data types
+    setValidations(newValidations)
+    setVList((value: any) => {
+      const newValidatorsList: any = { ...value }
+      newValidations.forEach((validation: any) => {
+        newValidatorsList[validation.pubkey] = {
+          ...value[validation.pubkey],
+          ledger_index: validation.ledger_index,
+          ledger_hash: validation.ledger_hash,
+        }
+      })
+      return newValidatorsList
+    })
+  }
+
+  const validatorCount = Object.keys(vList).length
+  return (
+    <div className="network-page">
+      <Streams
+        validators={vList}
+        updateValidators={updateValidators}
+        updateMetrics={setMetrics}
+      />
+
+      {validatorCount && (
+        // @ts-ignore - Work around for complex type assignment issues
+        <Hexagons data={validations} list={vList} />
+      )}
+      <div className="stat">
+        {validatorCount && (
+          <>
+            <span>{t('validators_found')}: </span>
+            <span>
+              {localizeNumber(validatorCount, language)}
+              {unlCount !== 0 && (
+                <i>
+                  {' '}
+                  ({t('unl')}: {unlCount})
+                </i>
+              )}
+            </span>
+          </>
+        )}
+      </div>
+      <div className="wrap">
+        <NetworkTabs selected="validators" />
+        <ValidatorsTable validators={vList} metrics={metrics} />
+      </div>
+    </div>
+  )
+}
