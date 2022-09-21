caeee730a29be7ed6ea36d28a617a42d95ea1f2c
jonathanlei <jonathanlei98@gmail.com>
2022-09-21 14:26:02 -0700

    delete file and fix types/ translation

--- src/containers/shared/components/Transaction/EscrowCancel/TableDetail.tsx
+++ src/containers/shared/components/Transaction/EscrowCancel/TableDetail.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { useTranslation } from 'react-i18next'
+import { useTranslation, Trans } from 'react-i18next'
 
 export const TableDetail = (props: any) => {
   const { t } = useTranslation()
@@ -8,13 +8,13 @@ export const TableDetail = (props: any) => {
   return (
     <div className="escrow">
       {owner && (
-        <div>
+        <Trans i18nKey="cancel_escrow">
           <span className="label">{t('cancel_escrow')}</span>
           <span className="account">{owner}</span>
           <span>
             -{sequence !== 0 ? sequence : `${ticketSequence} (Ticket)`}
           </span>
-        </div>
+        </Trans>
       )}
     </div>
   )
