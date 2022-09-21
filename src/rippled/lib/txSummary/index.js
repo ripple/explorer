2a8e0a15a5111b3d6113ebc3ea1b10d8fdc51c71
Connor Chen <67431889+connorjchen@users.noreply.github.com>
2022-09-21 15:50:14 -0400

    Fix PR specific

--- src/rippled/lib/txSummary/index.js
+++ src/rippled/lib/txSummary/index.js
@@ -16,6 +16,10 @@ const EnableAmendment = require('./EnableAmendment')
 const UNLModify = require('./UNLModify')
 const AccountDelete = require('./AccountDelete')
 const TicketCreate = require('./TicketCreate')
+const NFTokenAcceptOffer = require('./NFTokenAcceptOffer')
+const NFTokenBurn = require('./NFTokenBurn')
+const NFTokenCancelOffer = require('./NFTokenCancelOffer')
+const NFTokenMint = require('./NFTokenMint')
 
 const summarize = {
   OfferCancel,
@@ -33,6 +37,10 @@ const summarize = {
   UNLModify,
   AccountDelete,
   TicketCreate,
+  NFTokenAcceptOffer,
+  NFTokenBurn,
+  NFTokenCancelOffer,
+  NFTokenMint,
 }
 
 const getInstructions = (tx, meta) => {
