26a4760b3dcdcf29c034c21432e13bcd55968cfd
pdp2121 <71317875+pdp2121@users.noreply.github.com>
2022-09-22 16:01:53 -0400

    fix: reword nodes to validators bar chart (#352)
    Reword Upgrade Status chart labels from 'nodes' to 'validators'.

--- src/containers/Network/BarChartVersion.tsx
+++ src/containers/Network/BarChartVersion.tsx
@@ -35,7 +35,9 @@ const CustomTooltip = ({
       <div className="custom-tooltip">
         <p className="label">{t('version_display', { version: label })}</p>
         <p className="value">
-          {t('nodes_count', { count: payload ? payload[0].payload.count : 0 })}
+          {t('validators_count', {
+            count: payload ? payload[0].payload.count : 0,
+          })}
         </p>
       </div>
     )
@@ -90,11 +92,11 @@ const BarChartVersion = (props: Props) => {
           >
             <Label
               className="y-label"
-              value={t('%_of_total_nodes')}
+              value={t('%_of_total_validators')}
               angle={-90}
               position="insideTop"
               dx={45}
-              dy={65}
+              dy={80}
             />
           </YAxis>
           <Bar dataKey="value" barSize={30} fill={PURPLE} radius={[4, 4, 0, 0]}>
