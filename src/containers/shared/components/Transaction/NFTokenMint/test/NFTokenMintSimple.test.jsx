c6931d5854334fa0345438da854aac15966117ad
Connor Chen <67431889+connorjchen@users.noreply.github.com>
2022-09-26 16:06:58 -0400

    Change naming on test files

--- src/containers/shared/components/Transaction/NFTokenMint/test/NFTokenMintSimple.test.jsx
+++ src/containers/shared/components/Transaction/NFTokenMint/test/NFTokenMintSimple.test.jsx
@@ -3,19 +3,41 @@ import { BrowserRouter as Router } from 'react-router-dom'
 import { mount } from 'enzyme'
 import { I18nextProvider } from 'react-i18next'
 import { Simple as NFTokenMint } from '../Simple'
-import transaction1 from './mock_data/NFTokenMint1.json'
-import transaction2 from './mock_data/NFTokenMint2.json'
-import transaction3 from './mock_data/NFTokenMint3.json'
+import transactionModified2 from './mock_data/NFTokenMintModified2.json'
+import transactionModified1Created1 from './mock_data/NFTokenMintModified1Created1.json'
+import transactionModified2Created1 from './mock_data/NFTokenMintMostModified2Created1.json'
 import summarizeTransaction from '../../../../../../rippled/lib/txSummary'
 import i18n from '../../../../../../i18nTestConfig'
 
 describe('NFTokenMint', () => {
-  it('handles NFTokenMint simple view 1', () => {
+  it('handles NFTokenMint that modified 2 nodes', () => {
     const wrapper = mount(
       <I18nextProvider i18n={i18n}>
         <Router>
           <NFTokenMint
-            data={summarizeTransaction(transaction1, true).details}
+            data={summarizeTransaction(transactionModified2, true).details}
+          />
+        </Router>
+      </I18nextProvider>,
+    )
+    expect(wrapper.find('[data-test="token-id"] .value')).toHaveText(
+      '000800006203F49C21D5D6E022CB16DE3538F248662FC73C535743B40000001A',
+    )
+    expect(wrapper.find('[data-test="token-taxon"] .value')).toHaveText('1')
+    expect(wrapper.find('[data-test="token-uri"] .value')).toHaveText(
+      'https://gregweisbrod.com',
+    )
+    wrapper.unmount()
+  })
+
+  it('handles NFTokenMint that modified 1 node and created 1 node', () => {
+    const wrapper = mount(
+      <I18nextProvider i18n={i18n}>
+        <Router>
+          <NFTokenMint
+            data={
+              summarizeTransaction(transactionModified1Created1, true).details
+            }
           />
         </Router>
       </I18nextProvider>,
@@ -30,12 +52,14 @@ describe('NFTokenMint', () => {
     wrapper.unmount()
   })
 
-  it('handles NFTokenMint simple view 2', () => {
+  it('handles NFTokenMint that modified 2 nodes and created 1 node', () => {
     const wrapper = mount(
       <I18nextProvider i18n={i18n}>
         <Router>
           <NFTokenMint
-            data={summarizeTransaction(transaction2, true).details}
+            data={
+              summarizeTransaction(transactionModified2Created1, true).details
+            }
           />
         </Router>
       </I18nextProvider>,
@@ -48,27 +72,5 @@ describe('NFTokenMint', () => {
       'https://gregweisbrod.com',
     )
     wrapper.unmount()
-  })
-
-  describe('NFTokenMint', () => {
-    it('handles NFTokenMint simple view 3', () => {
-      const wrapper = mount(
-        <I18nextProvider i18n={i18n}>
-          <Router>
-            <NFTokenMint
-              data={summarizeTransaction(transaction3, true).details}
-            />
-          </Router>
-        </I18nextProvider>,
-      )
-      expect(wrapper.find('[data-test="token-id"] .value')).toHaveText(
-        '000800006203F49C21D5D6E022CB16DE3538F248662FC73C535743B40000001A',
-      )
-      expect(wrapper.find('[data-test="token-taxon"] .value')).toHaveText('1')
-      expect(wrapper.find('[data-test="token-uri"] .value')).toHaveText(
-        'https://gregweisbrod.com',
-      )
-      wrapper.unmount()
-    })
   })
 })
