0c9207197dd603b0cfea60879c0b183c44095878
Tushar Pardhe <tpardhe@gmail.com>
2022-09-28 19:56:21 +0100

    Fixed an issue occurred while merging

--- src/containers/shared/components/Transaction/index.ts
+++ src/containers/shared/components/Transaction/index.ts
@@ -31,9 +31,6 @@ import { XChainCommitTransaction as XChainCommit } from './XChainCommit'
 import { XChainCreateBridgeTransaction as XChainCreateBridge } from './XChainCreateBridge'
 import { XChainCreateClaimIDTransaction as XChainCreateClaimID } from './XChainCreateClaimID'
 import { XChainModifyBridgeTransaction as XChainModifyBridge } from './XChainModifyBridge'
-import { EscrowCreateTransaction as EscrowCreate } from './EscrowCreate'
-import { EscrowFinishTransaction as EscrowFinish } from './EscrowFinish'
-import { EscrowCancelTransaction as EscrowCancel } from './EscrowCancel'
 import { TransactionMapping } from './types'
 
 export const transactionTypes: { [key: string]: TransactionMapping } = {
@@ -70,7 +67,4 @@ export const transactionTypes: { [key: string]: TransactionMapping } = {
   XChainCreateBridge,
   XChainCreateClaimID,
   XChainModifyBridge,
-  EscrowCreate,
-  EscrowFinish,
-  EscrowCancel,
 }
