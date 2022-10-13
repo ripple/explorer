9954bab6315367762aac2b278d9c3c2640efb99c
Caleb Kniffen <ckniffen@ripple.com>
2022-10-13 15:13:18 -0500

    Switch AccountTransactionTable to react-query

--- src/containers/Accounts/AccountTransactionTable/test/AccountTransactionsTable.test.js
+++ src/containers/Accounts/AccountTransactionTable/test/AccountTransactionsTable.test.js
@@ -1,46 +1,44 @@
 import React from 'react'
 import { mount } from 'enzyme'
 import { I18nextProvider } from 'react-i18next'
-import { applyMiddleware, createStore } from 'redux'
-import thunk from 'redux-thunk'
-import { Provider } from 'react-redux'
+import { QueryClientProvider } from 'react-query'
 import { BrowserRouter as Router, Link } from 'react-router-dom'
-import rootReducer, { initialState } from '../../../../rootReducer'
 import i18n from '../../../../i18nTestConfig'
 import { AccountTransactionTable } from '../index'
 import TEST_TRANSACTIONS_DATA from './mockTransactions.json'
-import * as actionTypes from '../actionTypes'
-import { loadAccountTransactions } from '../actions'
+import { getAccountTransactions } from '../../../../rippled'
+import { queryClient } from '../../../shared/QueryClient'
 
-jest.mock('../actions', () => ({
+jest.mock('../../../../rippled', () => ({
   __esModule: true,
-  loadAccountTransactions: jest.fn(),
+  getAccountTransactions: jest.fn(),
 }))
 
 const TEST_ACCOUNT_ID = 'rTEST_ACCOUNT'
 
+function flushPromises() {
+  return new Promise((resolve) => setImmediate(resolve))
+}
+
 describe('AccountTransactionsTable container', () => {
   const createWrapper = (
-    state = {},
-    loadAccountTransactionsImpl = () => () => {},
+    getAccountTransactionsImpl = () =>
+      new Promise(
+        () => {},
+        () => {},
+      ),
   ) => {
-    loadAccountTransactions.mockImplementation(loadAccountTransactionsImpl)
-    const store = createStore(rootReducer, applyMiddleware(thunk))
+    getAccountTransactions.mockImplementation(getAccountTransactionsImpl)
     return mount(
-      <I18nextProvider i18n={i18n}>
-        <Provider store={store}>
+      <QueryClientProvider client={queryClient}>
+        <I18nextProvider i18n={i18n}>
           <Router>
             <AccountTransactionTable accountId={TEST_ACCOUNT_ID} />
           </Router>
-        </Provider>
-      </I18nextProvider>,
+        </I18nextProvider>
+      </QueryClientProvider>,
     )
   }
-
-  it('renders without crashing', () => {
-    const wrapper = createWrapper()
-    wrapper.unmount()
-  })
 
   it('renders static parts', () => {
     const wrapper = createWrapper()
@@ -49,34 +47,24 @@ describe('AccountTransactionsTable container', () => {
   })
 
   it('renders loader when fetching data', () => {
-    const state = { ...initialState }
-    state.accountTransactions.loading = true
-    const wrapper = createWrapper(state)
+    const wrapper = createWrapper()
     expect(wrapper.find('.loader').length).toBe(1)
     wrapper.unmount()
   })
 
   it('does not render loader if we have offline data', () => {
-    const state = {
-      ...initialState,
-      accountTransactions: {
-        loading: true,
-        data: TEST_TRANSACTIONS_DATA,
-      },
-    }
-    const wrapper = createWrapper(state)
+    const wrapper = createWrapper()
     expect(wrapper.find('.loader').length).toBe(1)
     wrapper.unmount()
   })
 
-  it('renders dynamic content with transaction data', (done) => {
-    const component = createWrapper({}, () => (dispatch) => {
-      dispatch({ type: actionTypes.FINISHED_LOADING_ACCOUNT_TRANSACTIONS })
-      dispatch({
-        type: actionTypes.ACCOUNT_TRANSACTIONS_LOAD_SUCCESS,
-        data: TEST_TRANSACTIONS_DATA,
-      })
-    })
+  it('renders dynamic content with transaction data', async () => {
+    const component = createWrapper(() =>
+      Promise.resolve(TEST_TRANSACTIONS_DATA),
+    )
+
+    await flushPromises()
+    component.update()
 
     expect(component.find('.load-more-btn').length).toBe(1)
     expect(component.find('.transaction-table').length).toBe(1)
@@ -84,6 +72,5 @@ describe('AccountTransactionsTable container', () => {
       1,
     )
     expect(component.find(Link).length).toBe(40)
-    done()
   })
 })
