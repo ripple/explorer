6c9d49a447a12096dbad934b397266f15d07778c
Shawn Xie <shawnxie920@gmail.com>
2022-10-25 18:17:28 -0400

    remove unnecessary bool

--- src/containers/shared/components/NFTokenLink.tsx
+++ src/containers/shared/components/NFTokenLink.tsx
@@ -2,17 +2,14 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 
 export interface NFTokenLinkProps {
-  link?: boolean
   tokenID: string
 }
 
 export const NFTokenLink = (props: NFTokenLinkProps) => {
-  const { link = true, tokenID } = props
-  return link ? (
+  const { tokenID } = props
+  return (
     <Link title={tokenID} to={`/nft/${tokenID}`}>
       {tokenID}
     </Link>
-  ) : (
-    <span title={tokenID}>{tokenID}</span>
   )
 }
