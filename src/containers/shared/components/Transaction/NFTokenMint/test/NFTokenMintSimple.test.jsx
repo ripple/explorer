c0d344d20cfc3796c4b2c45c9c6bfd47238ce4bd
Shawn Xie <shawnxie920@gmail.com>
2022-10-25 10:48:09 -0400

    test

--- src/containers/shared/components/Transaction/NFTokenMint/test/NFTokenMintSimple.test.jsx
+++ src/containers/shared/components/Transaction/NFTokenMint/test/NFTokenMintSimple.test.jsx
@@ -6,7 +6,7 @@ import { Simple as NFTokenMint } from '../Simple'
 import transactionModified2 from './mock_data/NFTokenMintModified2.json'
 import transactionModified1Created1 from './mock_data/NFTokenMintModified1Created1.json'
 import transactionModified2Created1 from './mock_data/NFTokenMintMostModified2Created1.json'
-import transactionModified3 from './mock_data/NFTokenMintModified3.json'
+import transactionModified3 from './mock_data/NFTokenMintModified4Created1.json'
 import summarizeTransaction from '../../../../../../rippled/lib/txSummary'
 import i18n from '../../../../../../i18nTestConfig'
 
@@ -90,7 +90,7 @@ describe('NFTokenMint', () => {
     )
     expect(wrapper.find('[data-test="token-taxon"] .value')).toHaveText('1')
     expect(wrapper.find('[data-test="token-uri"] .value')).toHaveText(
-      '516D5071416B3677777577796A71654C476F64665253375156774677394346736A6D363375485661556438387463',
+      'QmPqAk6wwuwyjqeLGodfRS7QVwFw9CFsjm63uHVaUd88tc',
     )
     wrapper.unmount()
   })
