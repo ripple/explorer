c3dc658fc1a9c87fee26f825a13671f3b2cd7d57
Shawn Xie <shawnxie920@gmail.com>
2022-10-26 17:09:43 -0400

    comment out tx

--- src/containers/NFT/NFTTabs/NFTTabs.tsx
+++ src/containers/NFT/NFTTabs/NFTTabs.tsx
@@ -4,14 +4,15 @@ import './styles.scss'
 import { Tabs } from '../../shared/components/Tabs'
 import { getBuyNFToffers, getSellNFToffers } from '../../../rippled/lib/rippled'
 import { Offers } from './Offers'
-import { Transactions } from './Transactions'
+// import { Transactions } from './Transactions'
 
 interface Props {
   tokenId: string
 }
 
 export const NFTTabs = (props: Props) => {
-  const { id = '', tab = 'transactions' } = useParams<{
+  // const { id = '', tab = 'transactions' } = useParams<{
+  const { id = '', tab = 'buy-offers' } = useParams<{
     id: string
     tab: string
   }>()
@@ -20,7 +21,7 @@ export const NFTTabs = (props: Props) => {
 
   function renderTabs() {
     const { path = '/' } = match
-    const tabs = ['transactions', 'buy-offers', 'sell-offers']
+    const tabs = ['buy-offers', 'sell-offers']
     // strips :url from the front and the identifier/tab info from the end
     const mainPath = [path.split('/:')[0], id].join('/')
     return <Tabs tabs={tabs} selected={tab} path={mainPath} />
@@ -37,7 +38,7 @@ export const NFTTabs = (props: Props) => {
             offerType="SellOffers"
           />
         )
-      case 'buy-offers':
+      default:
         return (
           <Offers
             key="BuyOffers"
@@ -46,10 +47,41 @@ export const NFTTabs = (props: Props) => {
             offerType="BuyOffers"
           />
         )
-      default:
-        return <Transactions tokenId={tokenId} />
     }
   }
+
+  // function renderTabs() {
+  //   const { path = '/' } = match
+  //   const tabs = ['transactions', 'buy-offers', 'sell-offers']
+  //   // strips :url from the front and the identifier/tab info from the end
+  //   const mainPath = [path.split('/:')[0], id].join('/')
+  //   return <Tabs tabs={tabs} selected={tab} path={mainPath} />
+  // }
+
+  // function renderTransactions() {
+  //   switch (tab) {
+  //     case 'sell-offers':
+  //       return (
+  //         <Offers
+  //           key="SellOffers"
+  //           tokenId={tokenId}
+  //           fetchOffers={getSellNFToffers}
+  //           offerType="SellOffers"
+  //         />
+  //       )
+  //     case 'buy-offers':
+  //       return (
+  //         <Offers
+  //           key="BuyOffers"
+  //           tokenId={tokenId}
+  //           fetchOffers={getBuyNFToffers}
+  //           offerType="BuyOffers"
+  //         />
+  //       )
+  //     default:
+  //       return <Transactions tokenId={tokenId} />
+  //   }
+  // }
 
   return (
     <div className="nft-tabs section">
--- src/containers/NFT/NFTTabs/test/NFTTabs.test.js
+++ src/containers/NFT/NFTTabs/test/NFTTabs.test.js
@@ -11,7 +11,8 @@ describe('NFT Transactions tab container', () => {
   const nftId =
     '000800011C7D8ED1D715A0017E41BF9499ECC17E7FB666320000099B00000000'
 
-  const createWrapper = (tab = 'transactions') =>
+  // const createWrapper = (tab = 'transactions') =>
+  const createWrapper = (tab = 'buy-offers') =>
     mount(
       <QueryClientProvider client={queryClient}>
         <I18nextProvider i18n={i18n}>
@@ -30,19 +31,29 @@ describe('NFT Transactions tab container', () => {
   it('renders table content', () => {
     const wrapper = createWrapper()
     expect(wrapper.find('.tabs').length).toBe(1)
-    expect(wrapper.find('a.tab').length).toBe(3)
-    expect(wrapper.find('a.tab').at(0).props().title).toBe('transactions')
-    expect(wrapper.find('a.tab').at(1).props().title).toBe('buy_offers')
-    expect(wrapper.find('a.tab').at(2).props().title).toBe('sell_offers')
-    expect(wrapper.find('a.tab.selected').text()).toEqual('transactions')
+    expect(wrapper.find('a.tab').length).toBe(2)
+    expect(wrapper.find('a.tab').at(0).props().title).toBe('buy_offers')
+    expect(wrapper.find('a.tab').at(1).props().title).toBe('sell_offers')
+    expect(wrapper.find('a.tab.selected').text()).toEqual('buy_offers')
     wrapper.unmount()
   })
 
-  it('renders transactions tab', () => {
-    const wrapper = createWrapper('transactions')
-    expect(wrapper.find('a.tab.selected').text()).toEqual('transactions')
-    wrapper.unmount()
-  })
+  // it('renders table content', () => {
+  //   const wrapper = createWrapper()
+  //   expect(wrapper.find('.tabs').length).toBe(1)
+  //   expect(wrapper.find('a.tab').length).toBe(3)
+  //   expect(wrapper.find('a.tab').at(0).props().title).toBe('transactions')
+  //   expect(wrapper.find('a.tab').at(1).props().title).toBe('buy_offers')
+  //   expect(wrapper.find('a.tab').at(2).props().title).toBe('sell_offers')
+  //   expect(wrapper.find('a.tab.selected').text()).toEqual('transactions')
+  //   wrapper.unmount()
+  // })
+
+  // it('renders transactions tab', () => {
+  //   const wrapper = createWrapper('transactions')
+  //   expect(wrapper.find('a.tab.selected').text()).toEqual('transactions')
+  //   wrapper.unmount()
+  // })
 
   it('renders buy offers tab', () => {
     const wrapper = createWrapper('buy-offers')
