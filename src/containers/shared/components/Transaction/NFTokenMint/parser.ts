d5a4e0b27eadbd96fec2fcd51e817b4d358bc268
Shawn Xie <shawnxie920@gmail.com>
2022-10-27 12:30:28 -0400

    cmts

--- src/containers/shared/components/Transaction/NFTokenMint/parser.ts
+++ src/containers/shared/components/Transaction/NFTokenMint/parser.ts
@@ -7,6 +7,16 @@ export const parser: TransactionParser<NFTokenMint, NFTokenMintInstructions> = (
   tx,
   meta,
 ) => {
+  // When a mint results in splitting an existing page,
+  // it results in a created page and a modified node. Sometimes,
+  // the created node needs to be linked to a third page, resulting
+  // in modifying that third page's PreviousPageMin or NextPageMin
+  // field changing, but no NFTs within that page changing. In this
+  // case, there will be no previous NFTs and we need to skip.
+  // However, there will always be NFTs listed in the final fields,
+  // as rippled outputs all fields in final fields even if they were
+  // not changed. Thus why we add the additional condition to check
+  // if the PreviousFields contains NFTokens
   const affectedNodes = meta.AffectedNodes.filter(
     (node: any) =>
       node.CreatedNode?.LedgerEntryType === 'NFTokenPage' ||
