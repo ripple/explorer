9dc3e81884b75780ce62c9bebeb64aaa250bbf62
Shawn Xie <shawnxie920@gmail.com>
2022-11-01 14:11:01 -0400

    add seq number

--- src/containers/NFT/NFTHeader/Details.tsx
+++ src/containers/NFT/NFTHeader/Details.tsx
@@ -15,7 +15,16 @@ interface Props {
 }
 
 export const Details = ({ data }: Props) => {
-  const { minted, domain, NFTTaxon, uri, transferFee, owner, isBurned } = data
+  const {
+    minted,
+    domain,
+    NFTTaxon,
+    uri,
+    transferFee,
+    owner,
+    isBurned,
+    NFTSequence,
+  } = data
   const { t } = useTranslation()
   const language = useLanguage()
   const formattedFee =
@@ -54,6 +63,10 @@ export const Details = ({ data }: Props) => {
         <tr className="row">
           <td className="col1">{t('transfer_fee')}</td>
           <td className="col2">{formattedFee}</td>
+        </tr>
+        <tr className="row">
+          <td className="col1">{t('sequence_number_short')}</td>
+          <td className="col2">{NFTSequence}</td>
         </tr>
         {isBurned && (
           <tr className="row">
