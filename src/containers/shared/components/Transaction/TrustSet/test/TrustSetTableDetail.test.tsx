e8e84fb14a84a22ec1fcfe881094d22a78147c79
Caleb Kniffen <ckniffen@ripple.com>
2022-11-16 16:06:32 -0600

    Fix copy-pasta in unit test

--- src/containers/shared/components/Transaction/TrustSet/test/TrustSetTableDetail.test.tsx
+++ src/containers/shared/components/Transaction/TrustSet/test/TrustSetTableDetail.test.tsx
@@ -1,13 +1,13 @@
 import { createTableDetailWrapperFactory } from '../../test'
 import { TableDetail } from '../TableDetail'
-import TicketCreate from './mock_data/TrustSet.json'
+import mockTrustSet from './mock_data/TrustSet.json'
 import i18n from '../../../../../../i18nTestConfig.en-US'
 
 const createWrapper = createTableDetailWrapperFactory(TableDetail, i18n)
 
 describe('TrustSet: TableDetail', () => {
   it('renders', () => {
-    const wrapper = createWrapper(TicketCreate)
+    const wrapper = createWrapper(mockTrustSet)
     expect(wrapper.find('.label')).toHaveText('Set Trust Limit')
     expect(wrapper.find('Amount')).toHaveText(
       'CN¥1,000,000,000.00 CNY.razqQKzJRdB4UxFPWf5NEpEG3WMkmwgcXA',
