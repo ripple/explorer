6cb7d1f50b656fb222ffb090688b6c58e5838207
Mayukha Vadari <mvadari@ripple.com>
2022-12-12 16:41:00 -0500

    get it to build

--- build-non-split.js
+++ build-non-split.js
@@ -5,6 +5,7 @@ const commitHash = require('child_process')
   .execSync('git rev-parse --short HEAD')
   .toString()
   .trim()
+const NodePolyfillPlugin = require('node-polyfill-webpack-plugin')
 
 const defaults = rewire('react-scripts/scripts/build.js')
 // eslint-disable-next-line no-underscore-dangle
@@ -49,3 +50,38 @@ config.plugins.push(
   }),
 )
 config.externals = ['dtrace-provider', 'fs']
+config.plugins.push(
+  new NodePolyfillPlugin({
+    excludeAliases: ['console', 'crypto'],
+  }),
+)
+config.plugins.push(
+  new webpack.ProvidePlugin({
+    process: 'process/browser',
+    Buffer: ['buffer', 'Buffer'],
+  }),
+)
+config.resolve.fallback = {
+  module: false,
+  dns: 'mock',
+  fs: false,
+  http2: false,
+  net: false,
+  tls: false,
+  child_process: false,
+  // Webpack no longer auto-polyfills for node core modules.
+  // These are the polyfills for the necessary modules.
+  assert: require.resolve('assert'),
+  crypto: false, // require.resolve('crypto-browserify'),
+  http: require.resolve('stream-http'),
+  https: require.resolve('https-browserify'),
+  os: require.resolve('os-browserify/browser'),
+  stream: require.resolve('stream-browserify'),
+  buffer: require.resolve('buffer/'),
+  console: require.resolve('console-browserify'),
+  path: require.resolve('path-browserify'),
+  url: 'url',
+  util: 'util',
+  zlib: 'browserify-zlib',
+}
+console.log(config)
