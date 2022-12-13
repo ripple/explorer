ddaba8f1dd6c9a8ce15d25fbf8a7eb9f50003981
Mayukha Vadari <mvadari@ripple.com>
2022-12-13 17:58:11 -0500

    fix more issues

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,12 @@ import { defineConfig } from 'vite'
 // import reactRefresh from '@vitejs/plugin-react-refresh'
 
 import react from '@vitejs/plugin-react'
-import viteTsconfigPaths from 'vite-tsconfig-paths'
+// import viteTsconfigPaths from 'vite-tsconfig-paths'
 import svgrPlugin from 'vite-plugin-svgr'
 import { createHtmlPlugin } from 'vite-plugin-html'
 // import replace from '@rollup/plugin-replace'
+import EnvironmentPlugin from 'vite-plugin-environment'
+import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -15,13 +17,21 @@ export default defineConfig({
     outDir: '../dist',
   },
   publicDir: '../public',
-  define: { 'process.env.NODE_DEBUG': '"false"' },
+  define: { global: {} },
   resolve: {
     alias: {
       process: 'process/browser',
       stream: 'stream-browserify',
       zlib: 'browserify-zlib',
-      util: 'util',
+      'util/': 'util',
+    },
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      // Node.js global to browser globalThis
+      define: {
+        global: 'globalThis',
+      },
     },
   },
   plugins: [
@@ -41,7 +51,12 @@ export default defineConfig({
         },
       },
     }),
-    viteTsconfigPaths(),
+    EnvironmentPlugin('all'),
+    NodeGlobalsPolyfillPlugin({
+      buffer: true,
+      process: true,
+    }),
+    // viteTsconfigPaths(),
     // replace({
     //   'process.env': 'import.meta.env',
     // }),
