342d9dac877e2a674e27259354619035806dfbf2
Mayukha Vadari <mvadari@ripple.com>
2023-01-04 19:34:45 -0500

    get some tests to pass!

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,17 +1,17 @@
-// import 'jest-enzyme'
+import 'jest-enzyme'
 import { configure } from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
 
-// const mockStorage = {}
+const mockStorage = {}
 
-// window.gtag = window.gtag || (() => true)
-// window.localStorage = window.localStorage || {
-//   getItem: (key) => mockStorage[key],
-//   setItem: (key, value) => {
-//     mockStorage[key] = value
-//   },
-//   removeItem: (key) => delete mockStorage[key],
-// }
+window.gtag = window.gtag || (() => true)
+window.localStorage = window.localStorage || {
+  getItem: (key) => mockStorage[key],
+  setItem: (key, value) => {
+    mockStorage[key] = value
+  },
+  removeItem: (key) => delete mockStorage[key],
+}
 configure({ adapter: new Adapter() })
 
 // jest.spyOn(console, 'error')
--- vite.config.js
+++ vite.config.js
@@ -109,6 +109,6 @@ export default defineConfig({
   test: {
     globals: true,
     environment: 'jsdom',
-    setupFiles: 'setupTests.js',
+    setupFiles: ['setupTests.js'],
   },
 })
