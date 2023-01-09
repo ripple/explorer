a87f0393a5404fd58be1725ebf38e581c88799d9
Mayukha Vadari <mvadari@ripple.com>
2023-01-09 11:02:48 -0500

    fix svg issue better

---
+++ svgTransform.js
@@ -0,0 +1,23 @@
+const path = require('path')
+
+// This is a custom Jest transformer turning file imports into filenames.
+// http://facebook.github.io/jest/docs/en/webpack.html
+
+module.exports = {
+  process(src, filename) {
+    const assetFilename = JSON.stringify(path.basename(filename))
+
+    return `const React = require('react');
+      module.exports = React.forwardRef((props, ref) => {
+        return {
+          $$typeof: Symbol.for('react.element'),
+          type: 'svg',
+          ref: ref,
+          key: null,
+          props: Object.assign({}, props, {
+            children: ${assetFilename}
+          })
+        };
+      });`
+  },
+}
