a344f0b42c7b3e5fc3fbf53a3001771a0690820e
Mayukha Vadari <mvadari@ripple.com>
2023-01-10 17:23:31 -0500

    remove use of bunyan

--- src/rippled/lib/logger.js
+++ src/rippled/lib/logger.js
@@ -1,21 +1,19 @@
-// import bunyan from 'bunyan'
-
-const log = (options) =>
-  // const logger = bunyan.createLogger(options)
-
-  ({
-    info: (...args) => {
-      // logger.info(...args)
-    },
-    warn: (...args) => {
-      // logger.warn(...args)
-    },
-    error: (...args) => {
-      // logger.error(...args)
-    },
-    debug: (...args) => {
-      // logger.debug(...args)
-    },
-  })
+/* eslint-disable no-console -- logging file */
+// TODO: refactor this file to only log in dev mode
+// and send logs in prod to the backend
+const log = (options) => ({
+  info: (message) => {
+    console.info({ type: 'info', ...options, message })
+  },
+  warn: (message) => {
+    console.warn({ type: 'warn', ...options, message })
+  },
+  error: (message) => {
+    console.error({ type: 'error', ...options, message })
+  },
+  debug: (message) => {
+    console.debug({ type: 'debug', ...options, message })
+  },
+})
 
 export default log
