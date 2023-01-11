053405f4fea81024f79af11cbdfbd8b4489b8793
Mayukha Vadari <mvadari@ripple.com>
2023-01-11 09:59:55 -0500

    JSON.stringify message

--- src/rippled/lib/logger.js
+++ src/rippled/lib/logger.js
@@ -3,16 +3,24 @@
 // and send logs in prod to the backend
 const log = (options) => ({
   info: (message) => {
-    console.info({ type: 'info', ...options, message })
+    console.info({ type: 'info', ...options, message: JSON.stringify(message) })
   },
   warn: (message) => {
-    console.warn({ type: 'warn', ...options, message })
+    console.warn({ type: 'warn', ...options, message: JSON.stringify(message) })
   },
   error: (message) => {
-    console.error({ type: 'error', ...options, message })
+    console.error({
+      type: 'error',
+      ...options,
+      message: JSON.stringify(message),
+    })
   },
   debug: (message) => {
-    console.debug({ type: 'debug', ...options, message })
+    console.debug({
+      type: 'debug',
+      ...options,
+      message: JSON.stringify(message),
+    })
   },
 })
 
