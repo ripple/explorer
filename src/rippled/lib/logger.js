ebdd10affdfc619902e3a58c2217713012c4781c
Mayukha Vadari <mvadari@ripple.com>
2023-01-20 16:13:19 -0500

    clean up logger

--- src/rippled/lib/logger.js
+++ src/rippled/lib/logger.js
@@ -1,27 +1,15 @@
 /* eslint-disable no-console -- logging file */
 // TODO: refactor this file to only log in dev mode
 // and send logs in prod to the backend
+const logMessage = (type, options, message) => {
+  console[type]({ ...options, message })
+}
+
 const log = (options) => ({
-  info: (message) => {
-    console.info({ type: 'info', ...options, message: JSON.stringify(message) })
-  },
-  warn: (message) => {
-    console.warn({ type: 'warn', ...options, message: JSON.stringify(message) })
-  },
-  error: (message) => {
-    console.error({
-      type: 'error',
-      ...options,
-      message: JSON.stringify(message),
-    })
-  },
-  debug: (message) => {
-    console.debug({
-      type: 'debug',
-      ...options,
-      message: JSON.stringify(message),
-    })
-  },
+  info: (message) => logMessage('info', options, message),
+  warn: (message) => logMessage('warn', options, message),
+  error: (message) => logMessage('error', options, message),
+  debug: (message) => logMessage('debug', options, message),
 })
 
 export default log
