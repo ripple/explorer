8f459d78ae15ce8f286f8f7ddd83eae8d32148e2
Mayukha Vadari <mvadari@ripple.com>
2023-03-23 18:10:57 -0400

    clean up

--- src/containers/shared/components/test/Account.test.tsx
+++ src/containers/shared/components/test/Account.test.tsx
@@ -11,86 +11,64 @@ const createWrapper = (component: JSX.Element) =>
     </I18nextProvider>,
   )
 
+const ACCOUNT = 'rHWcuuZoFvDS6gNbmHSdpb7u1hZzxvCoMt'
+
 describe('Account', () => {
   afterEach(cleanup)
   it('should render with a link', () => {
-    const account = 'rHWcuuZoFvDS6gNbmHSdpb7u1hZzxvCoMt'
-    createWrapper(<Account account={account} />)
+    createWrapper(<Account account={ACCOUNT} />)
     const link = screen.getByTestId('account')
     expect(link).toHaveClass('account')
-    expect(link).toHaveTextContent(account)
-    expect(link).toHaveAttribute('href', `/accounts/${account}`)
-    expect(link).toHaveAttribute('title', account)
+    expect(link).toHaveTextContent(ACCOUNT)
+    expect(link).toHaveAttribute('href', `/accounts/${ACCOUNT}`)
+    expect(link).toHaveAttribute('title', ACCOUNT)
     expect(screen.queryByTestId('dt')).toBeNull()
   })
   it('should render without a link', () => {
-    createWrapper(
-      <Account account="rHWcuuZoFvDS6gNbmHSdpb7u1hZzxvCoMt" link={false} />,
-    )
+    createWrapper(<Account account={ACCOUNT} link={false} />)
     const link = screen.getByTestId('account')
-    expect(link).toHaveTextContent('rHWcuuZoFvDS6gNbmHSdpb7u1hZzxvCoMt')
+    expect(link).toHaveTextContent(ACCOUNT)
     expect(link).not.toHaveAttribute('href')
-    expect(link).toHaveAttribute('title', 'rHWcuuZoFvDS6gNbmHSdpb7u1hZzxvCoMt')
+    expect(link).toHaveAttribute('title', ACCOUNT)
     expect(screen.queryByTestId('dt')).toBeNull()
   })
 
   it('should render with a destination tag', () => {
-    createWrapper(
-      <Account account="rHWcuuZoFvDS6gNbmHSdpb7u1hZzxvCoMt:381702" />,
-    )
+    createWrapper(<Account account={ACCOUNT} />)
     const link = screen.getByTestId('account')
     expect(link).toHaveClass('account')
-    expect(link).toHaveTextContent('rHWcuuZoFvDS6gNbmHSdpb7u1hZzxvCoMt')
-    expect(link).toHaveAttribute(
-      'href',
-      '/accounts/rHWcuuZoFvDS6gNbmHSdpb7u1hZzxvCoMt',
-    )
-    expect(link).toHaveAttribute('title', 'rHWcuuZoFvDS6gNbmHSdpb7u1hZzxvCoMt')
+    expect(link).toHaveTextContent(ACCOUNT)
+    expect(link).toHaveAttribute('href', `/accounts/${ACCOUNT}`)
+    expect(link).toHaveAttribute('title', ACCOUNT)
     expect(screen.queryByTestId('dt')).toHaveTextContent(':381702')
   })
 
   it('should render with a destination tag and no link', () => {
-    createWrapper(
-      <Account
-        account="rHWcuuZoFvDS6gNbmHSdpb7u1hZzxvCoMt:381702"
-        link={false}
-      />,
-    )
+    createWrapper(<Account account={ACCOUNT} link={false} />)
     const link = screen.getByTestId('account')
     expect(link).toHaveClass('account')
-    expect(link).toHaveTextContent('rHWcuuZoFvDS6gNbmHSdpb7u1hZzxvCoMt')
+    expect(link).toHaveTextContent(ACCOUNT)
     expect(link).not.toHaveAttribute('href')
-    expect(link).toHaveAttribute('title', 'rHWcuuZoFvDS6gNbmHSdpb7u1hZzxvCoMt')
+    expect(link).toHaveAttribute('title', ACCOUNT)
     expect(screen.queryByTestId('dt')).toHaveTextContent(':381702')
   })
 
   it('should render with a destination tag supplied separately', () => {
-    createWrapper(
-      <Account account="rHWcuuZoFvDS6gNbmHSdpb7u1hZzxvCoMt" tag={123} />,
-    )
+    createWrapper(<Account account={ACCOUNT} tag={123} />)
     const link = screen.getByTestId('account')
     expect(link).toHaveClass('account')
-    expect(link).toHaveTextContent('rHWcuuZoFvDS6gNbmHSdpb7u1hZzxvCoMt')
-    expect(link).toHaveAttribute(
-      'href',
-      '/accounts/rHWcuuZoFvDS6gNbmHSdpb7u1hZzxvCoMt',
-    )
-    expect(link).toHaveAttribute('title', 'rHWcuuZoFvDS6gNbmHSdpb7u1hZzxvCoMt')
+    expect(link).toHaveTextContent(ACCOUNT)
+    expect(link).toHaveAttribute('href', `/accounts/${ACCOUNT}`)
+    expect(link).toHaveAttribute('title', ACCOUNT)
     expect(screen.queryByTestId('dt')).toHaveTextContent(':123')
   })
 
   it('should render with a destination tag supplied separately and no link', () => {
-    createWrapper(
-      <Account
-        account="rHWcuuZoFvDS6gNbmHSdpb7u1hZzxvCoMt"
-        tag={123}
-        link={false}
-      />,
-    )
+    createWrapper(<Account account={ACCOUNT} tag={123} link={false} />)
     const link = screen.getByTestId('account')
-    expect(link).toHaveTextContent('rHWcuuZoFvDS6gNbmHSdpb7u1hZzxvCoMt')
+    expect(link).toHaveTextContent(ACCOUNT)
     expect(link).not.toHaveAttribute('href')
-    expect(link).toHaveAttribute('title', 'rHWcuuZoFvDS6gNbmHSdpb7u1hZzxvCoMt')
+    expect(link).toHaveAttribute('title', ACCOUNT)
     expect(screen.queryByTestId('dt')).toHaveTextContent(':123')
   })
 })
