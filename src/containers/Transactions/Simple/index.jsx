bc2a554638bc8dce888deda91e7b4a2f893f853c
Mayukha Vadari <mvadari@ripple.com>
2023-05-09 18:10:32 -0400

    fix linter

--- src/containers/Transactions/Simple/index.jsx
+++ src/containers/Transactions/Simple/index.jsx
@@ -1,11 +1,8 @@
-import { useTranslation } from 'react-i18next'
 import PropTypes from 'prop-types'
 import { transactionTypes } from '../../shared/components/Transaction'
 import { DefaultSimple } from '../../shared/components/Transaction/DefaultSimple'
 
 export const Simple = ({ data, type }) => {
-  const { t } = useTranslation()
-
   // Locate the component for the left side of the simple tab that is unique per TransactionType.
   const SimpleComponent = transactionTypes[type]?.Simple
   if (SimpleComponent) {
