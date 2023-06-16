ripple/explorer

commit b7eef5912898ec6d00557553743325652eb277eb

refactor: remove proptypes, use ts, and hooks (#742)
Cleans up more files by using hooks, removing proptypes and switching to
Typescript.

This leaves 5 components needing to be converted to hooks and 7 with
PropTypes. There are even more that need to use typescript most of those
are test files that will take a bit longer to sort out.

src/containers/Header/index.tsx
@@ -1,7 +1,7 @@
+import { FC } from 'react'
 import classnames from 'classnames'
-import PropTypes from 'prop-types'
 
-import Banner from './Banner'
+import { Banner } from './Banner'
 import { NavigationMenu } from './NavigationMenu'
 import { routesConfig } from './routes'
 
@@ -9,7 +9,7 @@ import './header.scss'
 import { LanguagePicker } from './LanguagePicker/LanguagePicker'
 import { NetworkPicker } from './NetworkPicker/NetworkPicker'
 
-export const Header = ({ inNetwork }) => (
+export const Header: FC<{ inNetwork?: boolean }> = ({ inNetwork }) => (
   <header className={classnames('header', !inNetwork && 'header-no-network')}>
     <div className="topbar">
       <NetworkPicker />
@@ -19,11 +19,3 @@ export const Header = ({ inNetwork }) => (
     <NavigationMenu routes={routesConfig} />
   </header>
 )
-
-Header.defaultProps = {
-  inNetwork: true,
-}
-
-Header.propTypes = {
-  inNetwork: PropTypes.bool,
-}

src/containers/Accounts/AccountHeader/index.tsx
@@ -5,7 +5,7 @@ import { bindActionCreators } from 'redux'
 import { Amount } from '../../shared/components/Amount'
 import { ExplorerAmount } from '../../shared/types'
 import { loadAccountState } from './actions'
-import Loader from '../../shared/components/Loader'
+import { Loader } from '../../shared/components/Loader'
 import './styles.scss'
 import { BalanceSelector } from './BalanceSelector/BalanceSelector'
 import { Account } from '../../shared/components/Account'

src/containers/Ledger/index.tsx
@@ -5,7 +5,7 @@ import { useParams } from 'react-router'
 import { Link } from 'react-router-dom'
 import { useQuery } from 'react-query'
 import NoMatch from '../NoMatch'
-import Loader from '../shared/components/Loader'
+import { Loader } from '../shared/components/Loader'
 import SocketContext from '../shared/SocketContext'
 import { useLanguage } from '../shared/hooks'
 import {

src/containers/PayStrings/PayStringMappingsTable/index.tsx
@@ -1,6 +1,6 @@
 import { useTranslation } from 'react-i18next'
 import { classicAddressToXAddress } from 'ripple-address-codec'
-import Loader from '../../shared/components/Loader'
+import { Loader } from '../../shared/components/Loader'
 import { PayStringResponse } from '../../../rippled/payString'
 import './styles.scss'
 

src/containers/Token/DEXPairs/index.tsx
@@ -3,7 +3,7 @@ import { useTranslation } from 'react-i18next'
 import axios from 'axios'
 import './styles.scss'
 import Log from '../../shared/log'
-import Loader from '../../shared/components/Loader'
+import { Loader } from '../../shared/components/Loader'
 import {
   formatLargeNumber,
   getLocalizedCurrencySymbol,

src/containers/Token/TokenHeader/index.tsx
@@ -4,7 +4,7 @@ import { useTranslation } from 'react-i18next'
 import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
 import { loadTokenState } from './actions'
-import Loader from '../../shared/components/Loader'
+import { Loader } from '../../shared/components/Loader'
 import './styles.scss'
 import { localizeNumber, formatLargeNumber } from '../../shared/utils'
 import SocketContext from '../../shared/SocketContext'

src/containers/Transactions/Description/index.tsx
@@ -1,6 +1,6 @@
 import { useTranslation } from 'react-i18next'
 import { FC } from 'react'
-import Sequence from '../../shared/components/Sequence'
+import { Sequence } from '../../shared/components/Sequence'
 import { transactionTypes } from '../../shared/components/Transaction'
 
 export const TransactionDescription: FC<{ data: any }> = ({ data }) => {

src/containers/Transactions/Meta/index.tsx
@@ -1,4 +1,4 @@
-import PropTypes from 'prop-types'
+import { FC } from 'react'
 import { useTranslation } from 'react-i18next'
 import renderAccountRoot from './AccountRoot'
 import renderDirectoryNode from './DirectoryNode'
@@ -14,7 +14,7 @@ const renderDefault = (t, action, node, index) => (
   </li>
 )
 
-export const TransactionMeta = ({ data }) => {
+export const TransactionMeta: FC<{ data: any }> = ({ data }) => {
   const language = useLanguage()
   const { t } = useTranslation()
 
@@ -22,17 +22,17 @@ export const TransactionMeta = ({ data }) => {
     const meta = list.map((node, index) => {
       switch (node.LedgerEntryType) {
         case 'AccountRoot':
-          return renderAccountRoot(t, language, action, node, index, tx)
+          return renderAccountRoot(t, language, action, node, index)
         case 'DirectoryNode':
-          return renderDirectoryNode(t, action, node, index, tx)
+          return renderDirectoryNode(t, action, node, index)
         case 'Offer':
           return renderOffer(t, language, action, node, index, tx)
         case 'RippleState':
-          return renderRippleState(t, language, action, node, index, tx)
+          return renderRippleState(t, language, action, node, index)
         case 'PayChannel':
-          return renderPayChannel(t, language, action, node, index, tx)
+          return renderPayChannel(t, language, action, node, index)
         default:
-          return renderDefault(t, action, node, index, tx)
+          return renderDefault(t, action, node, index)
       }
     })
 
@@ -61,15 +61,4 @@ export const TransactionMeta = ({ data }) => {
       {renderNodesMeta('deleted', affectedNodes.deleted, data.tx)}
     </div>
   )
-}
-
-TransactionMeta.propTypes = {
-  data: PropTypes.objectOf(
-    PropTypes.oneOfType([
-      PropTypes.string,
-      PropTypes.object,
-      PropTypes.number,
-      PropTypes.array,
-    ]),
-  ).isRequired,
 }

commit 6964733c5965890c6945ffd446ed8e422154b08f

fix: show search bar when endpoint is known (#747)
This was broken by #742 when the default value was removed.

Added tests to catch this in the future.

src/containers/Header/index.tsx
@@ -9,7 +9,7 @@ import './header.scss'
 import { LanguagePicker } from './LanguagePicker/LanguagePicker'
 import { NetworkPicker } from './NetworkPicker/NetworkPicker'
 
-export const Header: FC<{ inNetwork?: boolean }> = ({ inNetwork }) => (
+export const Header: FC<{ inNetwork?: boolean }> = ({ inNetwork = true }) => (
   <header className={classnames('header', !inNetwork && 'header-no-network')}>
     <div className="topbar">
       <NetworkPicker />

src/containers/App/test/App.test.jsx
@@ -129,6 +129,7 @@ describe('App container', () => {
     const wrapper = createWrapper()
     return new Promise((r) => setTimeout(r, 200)).then(() => {
       expect(document.title).toEqual('xrpl_explorer | ledgers')
+      expect(wrapper.find('header')).not.toHaveClassName('header-no-network')
       expect(wrapper.find('.ledgers').length).toBe(1)
       expect(window.dataLayer).toEqual([
         {
@@ -360,7 +361,19 @@ describe('App container', () => {
     })
   })
 
-  it('renders custom mode', async () => {
+  it('renders custom mode homepage', async () => {
+    process.env.VITE_ENVIRONMENT = 'custom'
+    delete process.env.VITE_P2P_RIPPLED_HOST //  For custom as there is no p2p.
+    const wrapper = createWrapper('/')
+    await flushPromises()
+    wrapper.update()
+    expect(wrapper.find('header')).toHaveClassName('header-no-network')
+    // We don't know the endpoint yet.
+    expect(XrplClient).toHaveBeenCalledTimes(0)
+    wrapper.unmount()
+  })
+
+  it('renders custom mode ledgers', async () => {
     process.env.VITE_ENVIRONMENT = 'custom'
     delete process.env.VITE_P2P_RIPPLED_HOST //  For custom as there is no p2p.
     const network = 's2.ripple.com'
@@ -368,6 +381,7 @@ describe('App container', () => {
     await flushPromises()
     wrapper.update()
     // Make sure the sockets aren't double initialized.
+    expect(wrapper.find('header')).not.toHaveClassName('header-no-network')
     expect(XrplClient).toHaveBeenCalledTimes(1)
     expect(localStorage.getItem(CUSTOM_NETWORKS_STORAGE_KEY)).toEqual(
       JSON.stringify([network]),