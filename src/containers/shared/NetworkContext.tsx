05e2cf6e3c76723179c7d6950242cae899ed17e2
Mayukha Vadari <mvadari@ripple.com>
2023-06-21 12:50:09 -0400

    update more variables

--- src/containers/shared/NetworkContext.tsx
+++ src/containers/shared/NetworkContext.tsx
@@ -6,9 +6,8 @@ const ENV_NETWORK_MAP: Record<string, string> = {
   mainnet: 'main',
   testnet: 'test',
   devnet: 'dev',
-  nft_sandbox: 'nft-dev',
   amm: 'amm-dev',
-  hooks: 'hooks-test',
+  hooks_testnet: 'hooks-test',
 }
 
 function getNetworkName() {
