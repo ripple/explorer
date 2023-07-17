f5209e4cad6df9798da2d3f68bd54c090e58421b
Mayukha Vadari <mvadari@gmail.com>
2023-07-17 14:40:47 -0700

    simplify hex2bin

--- src/containers/shared/components/Transaction/SetHook/utils.ts
+++ src/containers/shared/components/Transaction/SetHook/utils.ts
@@ -51,63 +51,12 @@ const maxTransactionValue: number = 103
 
 function hex2bin(input) {
   const hex = input.replace('0x', '').toLowerCase()
-  let out = ''
-  for (const c of hex) {
-    switch (c) {
-      case '0':
-        out += '0000'
-        break
-      case '1':
-        out += '0001'
-        break
-      case '2':
-        out += '0010'
-        break
-      case '3':
-        out += '0011'
-        break
-      case '4':
-        out += '0100'
-        break
-      case '5':
-        out += '0101'
-        break
-      case '6':
-        out += '0110'
-        break
-      case '7':
-        out += '0111'
-        break
-      case '8':
-        out += '1000'
-        break
-      case '9':
-        out += '1001'
-        break
-      case 'a':
-        out += '1010'
-        break
-      case 'b':
-        out += '1011'
-        break
-      case 'c':
-        out += '1100'
-        break
-      case 'd':
-        out += '1101'
-        break
-      case 'e':
-        out += '1110'
-        break
-      case 'f':
-        out += '1111'
-        break
-      default:
-        return ''
-    }
+  let bin = ''
+  for (let i = 0; i < hex.length; i += 1) {
+    const binFragment = parseInt(hex[i], 16).toString(2)
+    bin += binFragment.padStart(4, '0')
   }
-
-  return out
+  return bin
 }
 
 export function hookOnToTxList(hookOn?: string): string[] | undefined {
