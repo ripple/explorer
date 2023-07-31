f8c851d8c60f2d68d87802620cc37eb7dd524a4e
Luc Bocahut <luc@atlays.com>
2023-07-31 15:42:42 +0200

    add: French translation

--- src/i18n/baseConfig.ts
+++ src/i18n/baseConfig.ts
@@ -1,6 +1,6 @@
 import { InitOptions } from 'i18next'
 
-export const supportedLanguages = ['en-US', 'ja-JP']
+export const supportedLanguages = ['en-US', 'ja-JP', 'fr-FR']
 
 export const options: InitOptions = {
   returnNull: false,
