f045de405cc1ac65a9bda62e8b817e6470ad0303
Md Azad <mdazad15081999@gmail.com>
2023-08-29 07:29:05 +0530

    typescript and eslint error fixed

--- src/containers/Header/LanguagePicker/LanguagePicker.tsx
+++ src/containers/Header/LanguagePicker/LanguagePicker.tsx
@@ -5,18 +5,8 @@ import './LanguagePicker.scss'
 import { supportedLanguages } from '../../../i18n/baseConfig'
 
 export const LanguagePicker = () => {
-  const {t, i18n } = useTranslation()
-  const currentLanguage = useLanguage();
-
-
-  let lang:String;
-  if(currentLanguage=='en-US'){
-    lang  = "English";
-  }else if(currentLanguage =='ja-JP'){
-    lang = "Japanese";
-  }else{
-    lang = "Korean";
-  }
+  const { t, i18n } = useTranslation()
+  const currentLanguage = useLanguage()
 
   const handleLanguageClick = (language: string) => () => {
     i18n.changeLanguage(language)
@@ -31,13 +21,12 @@ export const LanguagePicker = () => {
         {supportedLanguages
           .filter((language) => language !== currentLanguage)
           .map((language) => (
-
             <DropdownItem
               key={language}
               className={`language-picker-${language}`}
               handler={handleLanguageClick(language)}
             >
-             {t(`language`, {
+              {t(`language`, {
                 context: language,
                 defaultValue: '',
               })}
