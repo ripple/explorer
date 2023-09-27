9317142fb8d866751f1ec8c810b0cdf393809ee0
Mayukha Vadari <mvadari@gmail.com>
2023-09-27 08:59:04 -0400

    add test

--- src/containers/shared/components/Transaction/test/DefaultSimple.test.tsx
+++ src/containers/shared/components/Transaction/test/DefaultSimple.test.tsx
@@ -22,7 +22,7 @@ describe('DefaultSimple', () => {
       'rHb9CJAWyB4rj91VRWn96DkukG4bwdtyTh',
     )
     expect(wrapper.find(`[data-test="Destination"] a`)).toExist()
-    expectSimpleRowText(wrapper, 'Amount', '\uE9001.00 XRP')
+    expectSimpleRowText(wrapper, 'Amount2', '\uE9001.00 XRP')
     expectSimpleRowText(wrapper, 'FinishAfter', '736447590')
     wrapper.unmount()
   })
