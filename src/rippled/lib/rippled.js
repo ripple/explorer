ripple/explorer

commit aade03b7465b95bc95f2d5ad5e6f04dda9f4065f

fix: always specify `api_version: 1` (#835)
`clio` defaults the `api_version` to `2` and `rippled` to `1`.

src/rippled/lib/rippled.js
@@ -24,7 +24,8 @@ const formatPaychannel = (d) => ({
 })
 
 const executeQuery = async (rippledSocket, params) =>
-  rippledSocket.send(params).catch((error) => {
+  // `clio` defaults the `api_version` to `2` and `rippled` to `1`.
+  rippledSocket.send({ ...params, api_version: 1 }).catch((error) => {
     const message =
       error.response && error.response.error_message
         ? error.response.error_message

commit 2698efd828a994000891bf81e5120a76199cf911

fix: show info for old tx
Viewing old transactions was broken by #835. When sending
`api_version: 1` to clio it will currently forward to a rippled node
which is not full history. That will be addresses soon but until then
wont send now that for every request.

Explicitly send api_version to get `account_info` because we only want
the result from the most recently validated ledger.

src/rippled/lib/rippled.js
@@ -24,8 +24,7 @@ const formatPaychannel = (d) => ({
 })
 
 const executeQuery = async (rippledSocket, params) =>
-  // `clio` defaults the `api_version` to `2` and `rippled` to `1`.
-  rippledSocket.send({ api_version: 1, ...params }).catch((error) => {
+  rippledSocket.send(params).catch((error) => {
     const message =
       error.response && error.response.error_message
         ? error.response.error_message
@@ -170,6 +169,7 @@ const getTransaction = (rippledSocket, txHash) => {
 const getAccountInfo = (rippledSocket, account) =>
   query(rippledSocket, {
     command: 'account_info',
+    api_version: 1,
     account,
     ledger_index: 'validated',
     signer_lists: true,