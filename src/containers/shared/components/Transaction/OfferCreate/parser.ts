e42ce57267f149c45e1b65fc2d235dd8cee99c6b
Mayukha Vadari <mvadari@ripple.com>
2023-10-02 19:03:26 -0400

    fix: fix OfferCreate price visual (#841)
    This PR fixes an issue where prices showed up weirdly on the
    `OfferCreate` price currency pair.
    
    Noticed while working on #839

--- src/containers/shared/components/Transaction/OfferCreate/parser.ts
+++ src/containers/shared/components/Transaction/OfferCreate/parser.ts
@@ -3,17 +3,20 @@ import { formatAmount } from '../../../../../rippled/lib/txSummary/formatAmount'
 
 export function parser(tx: any) {
   const gets = formatAmount(tx.TakerGets)
-  const base = tx.TakerGets.currency || 'XRP'
-  const counter = tx.TakerPays.currency || 'XRP'
+  const base = tx.TakerGets.currency ? tx.TakerGets : { currency: 'XRP' }
+  const counter = tx.TakerPays.currency ? tx.TakerPays : { currency: 'XRP' }
   const pays = formatAmount(tx.TakerPays)
   const price = pays.amount / gets.amount
-  const invert = CURRENCY_ORDER.indexOf(counter) > CURRENCY_ORDER.indexOf(base)
+  const invert =
+    CURRENCY_ORDER.indexOf(counter.currency) >
+    CURRENCY_ORDER.indexOf(base.currency)
 
   return {
     gets,
     pays,
     price: (invert ? 1 / price : price).toPrecision(6),
-    pair: invert ? `${counter}/${base}` : `${base}/${counter}`,
+    firstCurrency: invert ? counter : base,
+    secondCurrency: invert ? base : counter,
     cancel: tx.OfferSequence,
   }
 }
