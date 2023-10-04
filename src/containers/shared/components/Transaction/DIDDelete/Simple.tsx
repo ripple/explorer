5e048e2a8b936cc7247f0103138da75638edabcd
Mayukha Vadari <mvadari@ripple.com>
2023-10-04 10:28:32 -0400

    feat: add `DIDSet` and `DIDDelete` transactions (#838)
    ## High Level Overview of Change
    
    Title says it all.
    
    ### Context of Change
    
    https://github.com/XRPLF/rippled/pull/4636
    https://github.com/XRPLF/XRPL-Standards/discussions/100
    
    ### Type of Change
    
    - [x] New feature (non-breaking change which adds functionality)
    
    ### TypeScript/Hooks Update
    
    <!--
    In an effort to modernize the codebase, you should convert the files
    that you work with to React Hooks and TypeScript.
    If this is not possible (e.g. it's too many changes, touching too many
    files, etc.) please explain why here.
    -->
    
    - [ ] Updated files to React Hooks
    - [ ] Updated files to TypeScript
    
    ## Before / After
    
    <img width="964" alt="image"
    src="https://github.com/ripple/explorer/assets/8029314/e4448591-f416-46fb-bd58-7443b0c1856c">
    
    <img width="964" alt="image"
    src="https://github.com/ripple/explorer/assets/8029314/63379011-9d4b-4a39-bf0c-70bcc3320e67">
    
    <img width="964" alt="image"
    src="https://github.com/ripple/explorer/assets/8029314/ead656ce-1b48-44f6-bdca-5b031ae55806">
    
    ## Test Plan
    
    I added tests. CI passes.

---
+++ src/containers/shared/components/Transaction/DIDDelete/Simple.tsx
@@ -0,0 +1,8 @@
+import { TransactionSimpleComponent, TransactionSimpleProps } from '../types'
+import { DIDDelete } from './types'
+
+const Simple: TransactionSimpleComponent = (
+  _props: TransactionSimpleProps<DIDDelete>,
+) => null
+
+export { Simple }
---
+++ src/containers/shared/components/Transaction/DIDSet/test/DIDSetTableDetail.test.tsx
@@ -0,0 +1,14 @@
+import { createTableDetailWrapperFactory } from '../../test'
+import { TableDetail } from '../TableDetail'
+import DIDSet from './mock_data/DIDSet.json'
+
+const createWrapper = createTableDetailWrapperFactory(TableDetail)
+
+describe('DIDSet: TableDetail', () => {
+  it('renders', () => {
+    const wrapper = createWrapper(DIDSet)
+    // eslint-disable-next-line no-useless-concat -- easier to read this way
+    expect(wrapper).toHaveText('uri: did_example' + 'did_document: doc')
+    wrapper.unmount()
+  })
+})
