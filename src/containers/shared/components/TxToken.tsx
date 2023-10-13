26d923caf90c7e3a9d04312547725786117d2048
Mayukha Vadari <mvadari@gmail.com>
2023-10-13 12:26:04 -0400

    fix AMM transaction table currency display

--- src/containers/shared/components/TxToken.tsx
+++ src/containers/shared/components/TxToken.tsx
@@ -1,7 +1,18 @@
 import '../css/txlabel.scss'
+import { hexToString } from './Currency'
 
 interface Props {
   tx: any
+}
+
+const NON_STANDARD_CODE_LENGTH = 40
+const LP_TOKEN_IDENTIFIER = '03'
+
+function getCode(currency: string): string {
+  return currency?.length === NON_STANDARD_CODE_LENGTH &&
+    currency?.substring(0, 2) !== LP_TOKEN_IDENTIFIER
+    ? hexToString(currency)
+    : currency
 }
 
 // TODO: We should consider moving this logic to each individual parser. This would give us more customizability.
@@ -18,9 +29,13 @@ function getTokenPair(
     type === 'Payment'
   ) {
     const first =
-      amount?.amount && amount.amount !== fee ? amount.currency : undefined
+      amount?.amount && amount.amount !== fee
+        ? getCode(amount.currency)
+        : undefined
     const second =
-      amount2?.amount && amount2.amount !== fee ? amount2.currency : undefined
+      amount2?.amount && amount2.amount !== fee
+        ? getCode(amount2.currency)
+        : undefined
 
     if (first && second) {
       return first + (type === 'Payment' ? ' for ' : ' and ') + second
