37e0ed3c58c98f20977c306cb20a25b2507c7fae
Sohamm Kulkarni <kabeerkhb2020@gmail.com>
2023-10-17 06:31:44 +0000

    Refactor 'DomainLink' component to handle domain protocols
    updates the DomainLink component to handle domain protocols more effectively. It includes changes to check for the presence of protocols (http:// or https://), improving the handling of decoded domains, and ensuring the proper construction of href attributes.

--- src/containers/shared/components/DomainLink.tsx
+++ src/containers/shared/components/DomainLink.tsx
@@ -10,19 +10,14 @@ export interface Props {
 const DomainLink = (props: Props) => {
   const { className, decode = false, domain } = props
 
-  // Ensure that "https://" is not added twice
-  const domainWithoutProtocol = domain.replace(/^https?:\/\//, '')
-
   // If decode is true, decode the domain
-  const decodedDomain = decode
-    ? decodeHex(domainWithoutProtocol)
-    : domainWithoutProtocol
+  const decodedDomain = decode ? decodeHex(domain) : domain
 
-  // Construct the href based on the decoded or original domain
-  const href =
-    decodedDomain.startsWith('http://') || decodedDomain.startsWith('https://')
-      ? decodedDomain
-      : `https://${decodedDomain}`
+  // Check if the decodedDomain contains a protocol (http:// or https://)
+  const domainHasProtocol = decodedDomain.match(/^([a-z][a-z0-9+\-.]*):\/\//)
+
+  // if the decodedDomain already has a protocol, use it; oitherwise, add 'https://'
+  const href = domainHasProtocol ? decodedDomain : `https://${decodedDomain}`
 
   return (
     <a
