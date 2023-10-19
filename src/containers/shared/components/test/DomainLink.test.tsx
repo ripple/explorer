b8fc786640d384fc3d893dca5b4410f6f4ad74a6
Sohamm Kulkarni <kabeerkhb2020@gmail.com>
2023-10-19 16:32:43 +0000

    Refactor: Improved readability

--- src/containers/shared/components/test/DomainLink.test.tsx
+++ src/containers/shared/components/test/DomainLink.test.tsx
@@ -39,12 +39,12 @@ describe('DomainLink', () => {
     wrapper.unmount()
   })
   it('handles domain link with domain provided in HEX-encoded format', () => {
-    const wrapper = mount(
-      <DomainLink decode domain="68747470733A2F2F6578616D706C652E636F6D" />,
-    )
+    const url = 'https://example.com'
+    const urlInHex = '68747470733A2F2F6578616D706C652E636F6D'
+    const wrapper = mount(<DomainLink decode domain={urlInHex} />)
     expect(wrapper.find('a').props().className).toEqual('domain')
-    expect(wrapper.find('a').text()).toEqual('https://example.com')
-    expect(wrapper.find('a').props().href).toEqual('https://example.com')
+    expect(wrapper.find('a').text()).toEqual(url)
+    expect(wrapper.find('a').props().href).toEqual(url)
     wrapper.unmount()
   })
 })
