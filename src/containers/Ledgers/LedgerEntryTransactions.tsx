8f2877011b159bc4bce391a6730b260678c60056
Caleb Kniffen <ckniffen@ripple.com>
2024-01-22 16:59:49 -0600

    added hardcoded memoization checks

--- src/containers/Ledgers/LedgerEntryTransactions.tsx
+++ src/containers/Ledgers/LedgerEntryTransactions.tsx
@@ -1,24 +1,27 @@
+import { memo } from 'react'
 import { Loader } from '../shared/components/Loader'
 import { LedgerEntryTransaction } from './LedgerEntryTransaction'
 
 /**
  * A separate component to handle iterating over the transactions for a ledger on the homepage.
- * It is a separate component so that it can benefit from React's built in memoization as the array only changes once
+ * It is a separate component so that it can benefit from React's memoization the array only changes once
  * when the ledger closes and the call returns will all its transactions
  * @param transactions
  * @constructor
  */
-export const LedgerEntryTransactions = ({
-  transactions,
-}: {
-  transactions: any[]
-}) => (
-  <>
-    {transactions == null && <Loader />}
-    <div className="transactions">
-      {transactions?.map((tx) => (
-        <LedgerEntryTransaction transaction={tx} key={tx.hash} />
-      ))}
-    </div>
-  </>
+export const LedgerEntryTransactions = memo(
+  ({ transactions }: { transactions: any[] }) => (
+    <>
+      {transactions == null && <Loader />}
+      <div className="transactions">
+        {transactions?.map((tx) => (
+          <LedgerEntryTransaction transaction={tx} key={tx.hash} />
+        ))}
+      </div>
+    </>
+  ),
+  (prevProps, nextProps) =>
+    prevProps.transactions &&
+    nextProps.transactions &&
+    prevProps.transactions.length === nextProps.transactions.length,
 )
