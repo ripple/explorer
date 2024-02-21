ecee042833608690f5f36566ae6433ad08af6180
Mayukha Vadari <mvadari@gmail.com>
2024-02-21 18:07:13 -0500

    fix RippleState

--- src/containers/Transactions/DetailTab/Meta/RippleState.jsx
+++ src/containers/Transactions/DetailTab/Meta/RippleState.jsx
@@ -1,6 +1,7 @@
 import { Trans } from 'react-i18next'
 import { Account } from '../../../shared/components/Account'
 import { localizeNumber, computeBalanceChange } from '../../../shared/utils'
+import Currency from '../../../shared/components/Currency'
 
 const render = (t, language, action, node, index) => {
   const {
@@ -15,8 +16,11 @@ const render = (t, language, action, node, index) => {
 
   const line1 = (
     <Trans i18nKey="transaction_balance_line_one">
-      It {action} a <b>{currency}</b>
-      ripplestate node between
+      It {action} a{' '}
+      <b>
+        <Currency currency={currency} />
+      </b>
+      RippleState node between
       <Account account={account} />
       and
       <Account account={counterAccount} />
