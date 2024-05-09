ed4aed7f7856a86de99addc6e9732ebf5e17e8b0
Mayukha Vadari <mvadari@gmail.com>
2024-05-09 11:35:52 -0400

    fix linter issues

--- src/containers/shared/components/Transaction/UNLModify/test/UNLModifySimple.test.tsx
+++ src/containers/shared/components/Transaction/UNLModify/test/UNLModifySimple.test.tsx
@@ -1,4 +1,3 @@
-import { mount } from 'enzyme'
 import { cleanup, screen } from '@testing-library/react'
 import i18n from '../../../../../../i18n/testConfigEnglish'
 import { expectSimpleRowLabel, expectSimpleRowText } from '../../test'
@@ -7,9 +6,6 @@ import { createSimpleRenderFactory } from '../../test/createRenderFactory'
 import { Simple } from '../Simple'
 import mockUNLModifyEnable from './mock_data/UNLModifyEnable.json'
 import mockUNLModifyDisable from './mock_data/UNLModifyDisable.json'
-import { SimpleTab } from '../../../../../Transactions/SimpleTab'
-import { QuickHarness } from '../../../../../test/utils'
-import summarizeTransaction from '../../../../../../rippled/lib/txSummary'
 
 const renderComponent = createSimpleRenderFactory(Simple, i18n)
 
@@ -40,17 +36,7 @@ describe('UNLModify: Simple', () => {
   })
 
   it('renders tx with correct account and sequence', () => {
-    const screen = mount(
-      <QuickHarness i18n={i18n}>
-        <SimpleTab
-          data={{
-            raw: mockUNLModifyDisable,
-            summary: summarizeTransaction(mockUNLModifyDisable, true).details,
-          }}
-          width={800}
-        />
-      </QuickHarness>,
-    )
+    renderComponent(mockUNLModifyDisable)
     expect(screen.getByTestId('account')).not.toExist()
     expectSimpleRowLabel(screen, 'sequence', 'Sequence Number')
     expectSimpleRowText(screen, 'sequence', '0')
