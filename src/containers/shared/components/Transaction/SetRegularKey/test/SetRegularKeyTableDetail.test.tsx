5f9e82d075830aacba559cd4eb6cfe5586a3658e
Mayukha Vadari <mvadari@gmail.com>
2024-05-09 12:47:26 -0400

    fix stuff

--- src/containers/shared/components/Transaction/SetRegularKey/test/SetRegularKeyTableDetail.test.tsx
+++ src/containers/shared/components/Transaction/SetRegularKey/test/SetRegularKeyTableDetail.test.tsx
@@ -1,4 +1,4 @@
-import { cleanup, screen } from '@testing-library/react'
+import { cleanup } from '@testing-library/react'
 import SetRegularKey from './mock_data/SetRegularKey.json'
 import SetRegularKeyUnset from './mock_data/SetRegularKeyUnsetKey.json'
 import { TableDetail } from '../TableDetail'
@@ -9,19 +9,14 @@ const renderComponent = createTableDetailRenderFactory(TableDetail)
 describe('SetRegularKeyTable: TableDetail', () => {
   afterEach(cleanup)
   it('renders Simple for transaction', () => {
-    renderComponent(SetRegularKey)
+    const { container } = renderComponent(SetRegularKey)
 
-    expect(screen.find('.setregularkey')).toBeDefined()
-    expect(screen.find('.label').text()).toBe(`regular_key`)
-    expect(screen.find('.key').text()).toBe(
-      `rULyyLRoZ47P33Vapew67VoiRqPrZ2ejbp`,
-    )
+    expect(container).toHaveTextContent(`rULyyLRoZ47P33Vapew67VoiRqPrZ2ejbp`)
   })
 
   it('renders Simple for transaction that unsets key', () => {
-    renderComponent(SetRegularKeyUnset)
+    const { container } = renderComponent(SetRegularKeyUnset)
 
-    expect(screen.find('.unset')).toBeDefined()
-    expect(screen.text()).toBe(`unset_regular_key`)
+    expect(container).toHaveTextContent(`unset_regular_key`)
   })
 })
