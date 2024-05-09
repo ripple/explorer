0cc0d0d5260089f65254c7097feefe5c60a956d7
Mayukha Vadari <mvadari@gmail.com>
2024-05-09 12:01:41 -0400

    fix more issues

--- src/containers/shared/components/Transaction/AMMDelete/test/AMMDeleteDescription.test.tsx
+++ src/containers/shared/components/Transaction/AMMDelete/test/AMMDeleteDescription.test.tsx
@@ -14,7 +14,7 @@ describe('AMMDelete: Description', () => {
     expect(screen.getByTestId('amm-delete-description')).toHaveTextContent(
       'Attempted to delete the AMM for \uE900 XRP and FOO.rm5c42Crqpdch5fbuCdHmSMV1wrL9arV9.If there were more than 512 trustlines, this only removes 512 trustlines instead.',
     )
-    expect(screen.findByRole('link')).toHaveAttribute(
+    expect(screen.queryByRole('link')).toHaveAttribute(
       'href',
       '/token/FOO.rm5c42Crqpdch5fbuCdHmSMV1wrL9arV9',
     )
