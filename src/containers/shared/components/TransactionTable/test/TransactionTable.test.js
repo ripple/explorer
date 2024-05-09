3a23d664c91b4dd7a65b474d0975e57c1793e227
Mayukha Vadari <mvadari@gmail.com>
2024-05-09 16:58:46 -0400

    convert more test files

--- src/containers/shared/components/TransactionTable/test/TransactionTable.test.js
+++ src/containers/shared/components/TransactionTable/test/TransactionTable.test.js
@@ -1,4 +1,4 @@
-import { mount } from 'enzyme'
+import { cleanup, render, screen } from '@testing-library/react'
 import { I18nextProvider } from 'react-i18next'
 import { BrowserRouter } from 'react-router-dom'
 import { TransactionTable } from '../TransactionTable'
@@ -8,14 +8,15 @@ import mockTx from './mockTransactions.json'
 const loadMore = jest.fn()
 
 describe('Transaction Table container', () => {
-  const createWrapper = (
+  afterEach(cleanup)
+  const renderComponent = (
     transactions = [],
     emptyMessage = undefined,
     loading = false,
     onLoadMore = loadMore,
     hasAdditionalResults = false,
   ) =>
-    mount(
+    render(
       <I18nextProvider i18n={i18n}>
         <BrowserRouter>
           <TransactionTable
@@ -30,70 +31,40 @@ describe('Transaction Table container', () => {
     )
 
   it('renders without crashing', () => {
-    const wrapper = createWrapper()
-    wrapper.unmount()
+    renderComponent()
   })
 
   it('renders multi-page content', () => {
-    const wrapper = createWrapper(
-      mockTx.transactions,
-      undefined,
-      false,
-      loadMore,
-      false,
-    )
+    renderComponent(mockTx.transactions, undefined, false, loadMore, false)
 
     expect(wrapper.find('.transaction-table').length).toBe(1)
     expect(wrapper.find('.upper').length).toBe(3)
     expect(wrapper.find('.details').length).toBe(2)
     expect(wrapper.find('.load-more-btn').length).toEqual(0)
-    wrapper.unmount()
   })
 
   it('renders single-page content', () => {
-    const wrapper = createWrapper(
-      mockTx.transactions,
-      undefined,
-      false,
-      loadMore,
-      true,
-    )
+    renderComponent(mockTx.transactions, undefined, false, loadMore, true)
     expect(wrapper.find('.transaction-table').length).toBe(1)
     expect(wrapper.find('.upper').length).toBe(3)
     expect(wrapper.find('.details').length).toBe(2)
     expect(wrapper.find('.load-more-btn').length).toEqual(1)
-    wrapper.unmount()
   })
 
   it('renders without details', () => {
-    const wrapper = createWrapper(
-      mockTx.transactions,
-      undefined,
-      false,
-      loadMore,
-      true,
-    )
+    renderComponent(mockTx.transactions, undefined, false, loadMore, true)
     expect(wrapper.find('.transaction-table').length).toBe(1)
     expect(wrapper.find('.upper').length).toBe(3)
     expect(wrapper.find('.details').length).toBe(2)
-    wrapper.unmount()
   })
 
   it('renders loader', () => {
-    const wrapper = createWrapper(
-      mockTx.transactions,
-      undefined,
-      true,
-      loadMore,
-      false,
-    )
+    renderComponent(mockTx.transactions, undefined, true, loadMore, false)
     expect(wrapper.find('.loader').length).toBe(1)
-    wrapper.unmount()
   })
 
   it('renders empty message', () => {
-    const wrapper = createWrapper([], undefined, false, loadMore, false)
+    renderComponent([], undefined, false, loadMore, false)
     expect(wrapper.find('.empty-transactions-message').length).toBe(1)
-    wrapper.unmount()
   })
 })
