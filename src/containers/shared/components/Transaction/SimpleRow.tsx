72fb3ceb7349a046d785e42e47ec186c49bbd951
Mayukha Vadari <mvadari@gmail.com>
2024-05-09 12:01:41 -0400

    fix more issues

--- src/containers/shared/components/Transaction/SimpleRow.tsx
+++ src/containers/shared/components/Transaction/SimpleRow.tsx
@@ -4,12 +4,13 @@ import classnames from 'classnames'
 export type SimpleRowProps = PropsWithChildren<{
   className?: string
   label: string
+  'data-testid'?: string
 }>
 
 export const SimpleRow = (props: SimpleRowProps) => {
-  const { label, children, className } = props
+  const { label, children, className, 'data-testid': testId } = props
   return (
-    <div className="row" data-testid={label}>
+    <div className="row" data-testid={testId ?? label}>
       <div className="label">{label}</div>
       <div className={classnames(`value`, className)}>{children}</div>
     </div>
