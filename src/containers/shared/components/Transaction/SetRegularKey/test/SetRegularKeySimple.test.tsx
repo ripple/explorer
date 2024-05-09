77cd098f762347d96f0ddd17557dab6fc04f2a4d
Mayukha Vadari <mvadari@gmail.com>
2024-05-09 12:47:26 -0400

    fix stuff

--- src/containers/shared/components/Transaction/SetRegularKey/test/SetRegularKeySimple.test.tsx
+++ src/containers/shared/components/Transaction/SetRegularKey/test/SetRegularKeySimple.test.tsx
@@ -2,8 +2,8 @@ import { cleanup, screen } from '@testing-library/react'
 import SetRegularKey from './mock_data/SetRegularKey.json'
 import SetRegularKeyUnset from './mock_data/SetRegularKeyUnsetKey.json'
 import { Simple } from '../Simple'
-import { SimpleRow } from '../../SimpleRow'
 import { createSimpleRenderFactory } from '../../test/createRenderFactory'
+import { expectSimpleRowLabel, expectSimpleRowText } from '../../test'
 
 const renderComponent = createSimpleRenderFactory(Simple)
 
@@ -11,19 +11,18 @@ describe('SetRegularKey: Simple', () => {
   afterEach(cleanup)
   it('renders Simple for transaction', () => {
     renderComponent(SetRegularKey)
-    const keyRow = screen.find(SimpleRow)
-
-    expect(keyRow.prop('label')).toBe(`regular_key`)
-    expect(keyRow.find('.value').text()).toBe(
-      `rULyyLRoZ47P33Vapew67VoiRqPrZ2ejbp`,
+    expectSimpleRowLabel(screen, 'regular_key', 'regular_key')
+    expectSimpleRowText(
+      screen,
+      'regular_key',
+      'rULyyLRoZ47P33Vapew67VoiRqPrZ2ejbp',
     )
   })
 
   it('renders Simple for transaction that unsets key', () => {
     renderComponent(SetRegularKeyUnset)
-    const keyRow = screen.find(SimpleRow)
 
-    expect(keyRow.prop('label')).toBe('')
-    expect(keyRow.find('.unset').hostNodes().text()).toBe(`unset_regular_key`)
+    expectSimpleRowLabel(screen, 'unset', 'unset')
+    expectSimpleRowText(screen, 'unset', 'unset_regular_key')
   })
 })
