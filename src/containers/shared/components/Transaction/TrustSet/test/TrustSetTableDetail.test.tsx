99449e580b53b893d60f0a13e974079fb8b68e0b
Mayukha Vadari <mvadari@gmail.com>
2024-05-09 12:55:24 -0400

    fix T tests

--- src/containers/shared/components/Transaction/TrustSet/test/TrustSetTableDetail.test.tsx
+++ src/containers/shared/components/Transaction/TrustSet/test/TrustSetTableDetail.test.tsx
@@ -1,8 +1,5 @@
-import { cleanup, screen } from '@testing-library/react'
-import {
-  createTableDetailRenderFactory,
-  expectSimpleRowLabel,
-} from '../../test'
+import { cleanup } from '@testing-library/react'
+import { createTableDetailRenderFactory } from '../../test'
 import { TableDetail } from '../TableDetail'
 import mockTrustSet from './mock_data/TrustSet.json'
 import i18n from '../../../../../../i18n/testConfigEnglish'
@@ -12,9 +9,9 @@ const renderComponent = createTableDetailRenderFactory(TableDetail, i18n)
 describe('TrustSet: TableDetail', () => {
   afterEach(cleanup)
   it('renders', () => {
-    renderComponent(mockTrustSet)
-    expectSimpleRowLabel(screen, 'Set Trust Limit', 'Set Trust Limit')
-    expect(screen.getByTestId('Amount')).toHaveTextContent(
+    const { container } = renderComponent(mockTrustSet)
+    expect(container).toHaveTextContent('Set Trust Limit')
+    expect(container).toHaveTextContent(
       'CN¥1,000,000,000.00 CNY.razqQKzJRdB4UxFPWf5NEpEG3WMkmwgcXA',
     )
   })
