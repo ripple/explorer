5f660c5d2979142c966850a5449606b799cdaa35
Lathan Britz <jucallme@gmail.com>
2024-06-26 15:32:11 -0400

    add tests

--- src/containers/Network/NodesTable.tsx
+++ src/containers/Network/NodesTable.tsx
@@ -165,7 +165,7 @@ export const NodesTable: FC<{ nodes: NodeData[] }> = ({
   const compareSemanticVersions = (
     a: string,
     b: string,
-    returnValue: string,
+    returnValue: number,
   ) => {
     const a1 = a.split('.')
     const b1 = b.split('.')
