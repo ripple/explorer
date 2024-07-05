db7aa73cd8ce10c7a54f30a85794e8076919d096
Mayukha Vadari <mvadari@gmail.com>
2024-07-05 11:47:58 -0400

    update new tests

--- src/containers/shared/components/Transaction/OracleSet/test/OracleSetTableDetail.test.tsx
+++ src/containers/shared/components/Transaction/OracleSet/test/OracleSetTableDetail.test.tsx
@@ -1,19 +1,20 @@
-import { createTableDetailWrapperFactory } from '../../test'
+import { cleanup, screen } from '@testing-library/react'
+import { createTableDetailRenderFactory } from '../../test'
 import { TableDetail } from '../TableDetail'
 import OracleSet from './mock_data/OracleSet.json'
 
-const createWrapper = createTableDetailWrapperFactory(TableDetail)
+const renderComponent = createTableDetailRenderFactory(TableDetail)
 
 describe('OracleDelete: TableDetail', () => {
+  afterEach(cleanup)
   it('renders', () => {
-    const wrapper = createWrapper(OracleSet)
-    expect(wrapper).toHaveText(
+    renderComponent(OracleSet)
+    expect(screen.getByTestId('table-detail')).toHaveTextContent(
       'oracle_document_id: 1' +
         'provider: provider' +
         'asset_class: currency' +
         'last_update_time: May 13, 2024 at 9:05:10 PM' +
         'trading_pairs: 74.2\uE900 XRP/USD, 1.03BTC/AUDT',
     )
-    wrapper.unmount()
   })
 })
