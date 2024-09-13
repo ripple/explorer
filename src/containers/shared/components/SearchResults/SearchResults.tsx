cc3ef2066aa72efbb634a363b352a5ec9d9261e2
Ashray Chowdhry <achowdhry@ripple.com>
2024-09-13 16:26:53 -0400

    minor pathves

--- src/containers/shared/components/SearchResults/SearchResults.tsx
+++ src/containers/shared/components/SearchResults/SearchResults.tsx
@@ -1,7 +1,6 @@
 import { useContext, useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
 import { convertHexToString } from 'xrpl'
-import { getRoute } from '../../../Header/Search'
 import Logo from '../../images/generic_token.svg'
 import './SearchResults.scss'
 
@@ -14,70 +13,39 @@ interface SearchResultsProps {
 
 let socket
 
-const SearchResultBarNonToken = ({ type, path, onClick }) => {
-  const generateBarText = () => {
-    switch (type) {
-      case 'ledgers':
-        return 'Search ledger ID'
-      case 'accounts':
-        return 'Search account'
-      case 'paystrings':
-        return 'Search paystring'
-      case 'token':
-        return 'Search token'
-      case 'validators':
-        return 'Search validators'
-      case 'transactions':
-        return 'Search transactions'
-      default:
-        return null
+const SearchResultBar = ({ resultContent, onClick }) => {
+  const parsePrice = (price) => {
+    const parsed = Number(price).toFixed(6)
+    if (Number(parsed) === 0) {
+      return '~$0'
     }
+    return `$${parseFloat(parsed)}`
   }
-
   return (
-    <Link to={path} onClick={onClick}>
+    <Link
+      to={`/token/${resultContent.currency}.${resultContent.issuer}`}
+      onClick={onClick}
+    >
       <div className="search-result-row">
-        <Logo
-          style={{
-            width: '1.5rem',
-            height: '1.5rem',
-            margin: '1rem',
-            marginRight: '50px',
-            alignContent: 'center',
-            justifyContent: 'center',
-          }}
-        />
-        <div
-          style={{
-            marginRight: '30px',
-            width: '200px',
-            alignContent: 'center',
-            justifyContent: 'center',
-          }}
-        >
-          <h4>{generateBarText()}</h4>
-        </div>
-      </div>
-    </Link>
-  )
-}
-
-const SearchResultBar = ({ resultContent, onClick }) => (
-  <Link
-    to={`/token/${resultContent.currency}.${resultContent.issuer}`}
-    onClick={onClick}
-  >
-    <div className="search-result-row">
-      <div className="search-result-logo">
-        {resultContent.meta.token.icon ? (
-          <object
-            data={resultContent.meta.token.icon}
-            style={{
-              width: '1.5rem',
-              height: '1.5rem',
-              borderRadius: '16px',
-            }}
-          >
+        <div className="search-result-logo">
+          {resultContent.meta.token.icon ? (
+            <object
+              data={resultContent.meta.token.icon}
+              style={{
+                width: '1.5rem',
+                height: '1.5rem',
+                borderRadius: '16px',
+              }}
+            >
+              <Logo
+                style={{
+                  width: '1.5rem',
+                  height: '1.5rem',
+                  borderRadius: '16px',
+                }}
+              />
+            </object>
+          ) : (
             <Logo
               style={{
                 width: '1.5rem',
@@ -85,73 +53,70 @@ const SearchResultBar = ({ resultContent, onClick }) => (
                 borderRadius: '16px',
               }}
             />
-          </object>
-        ) : (
-          <Logo
-            style={{
-              width: '1.5rem',
-              height: '1.5rem',
-              borderRadius: '16px',
-            }}
-          />
-        )}
-      </div>
-      <div className="search-result-content">
-        <div className="search-result-row-line-one">
-          <div style={{ paddingTop: '2px', paddingBottom: '2px' }}>
-            {resultContent.currency.length > 10
-              ? convertHexToString(resultContent.currency)
-              : resultContent.currency}
+          )}
+        </div>
+        <div className="search-result-content">
+          <div className="search-result-row-line-one">
+            <div
+              style={{
+                paddingTop: '2px',
+                paddingBottom: '2px',
+              }}
+            >
+              {resultContent.currency.length > 10
+                ? convertHexToString(resultContent.currency)
+                : resultContent.currency}
+            </div>
+            <div
+              style={{
+                paddingTop: '2px',
+                paddingBottom: '2px',
+                marginLeft: '3px',
+              }}
+            >
+              {resultContent.meta.token.name && (
+                <div>({resultContent.meta.token.name.trim()})</div>
+              )}
+            </div>
+            <div className="search-result-metric-chip">
+              {parsePrice(resultContent.metrics.price)}
+            </div>
+            <div className="search-result-metric-chip">
+              HOLDERS: {resultContent.metrics.holders}
+            </div>
+            <div className="search-result-metric-chip">
+              TRUSTLINES: {resultContent.metrics.trustlines}
+            </div>
           </div>
-          <div
-            style={{
-              paddingTop: '2px',
-              paddingBottom: '2px',
-              marginLeft: '0.25rem',
-            }}
-          >
-            {resultContent.meta.token.name && (
-              <div>({resultContent.meta.token.name})</div>
+          <div className="search-result-row-line-two">
+            <div>Address:</div>
+            {resultContent.issuer && (
+              <Link
+                to={`/accounts/${resultContent.issuer}`}
+                className="issuer-link"
+              >
+                <div>{resultContent.issuer}</div>
+              </Link>
             )}
           </div>
-          <div className="search-result-metric-chip">
-            ~${resultContent.metrics.price.substring(0, 7)}
+          <div className="search-result-row-line-three">
+            <div>Website:</div>
+            {resultContent.meta.issuer.domain && (
+              <a
+                href={`https://${resultContent.meta.issuer.domain}/`}
+                target="_blank"
+                rel="noreferrer"
+                className="website-link"
+              >
+                <div>{resultContent.meta.issuer.domain}</div>
+              </a>
+            )}
           </div>
-          <div className="search-result-metric-chip">
-            HOLDERS: {resultContent.metrics.holders}
-          </div>
-          <div className="search-result-metric-chip">
-            TRUSTLINES: {resultContent.metrics.trustlines}
-          </div>
-        </div>
-        <div className="search-result-row-line-two">
-          <div>Address:</div>
-          {resultContent.issuer && (
-            <Link
-              to={`/accounts/${resultContent.issuer}`}
-              className="issuer-link"
-            >
-              <div>{resultContent.issuer}</div>
-            </Link>
-          )}
-        </div>
-        <div className="search-result-row-line-three">
-          <div>Website:</div>
-          {resultContent.meta.issuer.domain && (
-            <a
-              href={`https://${resultContent.meta.issuer.domain}/`}
-              target="_blank"
-              rel="noreferrer"
-              className="website-link"
-            >
-              <div>{resultContent.meta.issuer.domain}</div>
-            </a>
-          )}
         </div>
       </div>
-    </div>
-  </Link>
-)
+    </Link>
+  )
+}
 
 const SearchResults = ({
   currentSearchValue,
@@ -159,7 +124,7 @@ const SearchResults = ({
 }: SearchResultsProps): JSX.Element => {
   const xrplSocket = useContext(SocketContext)
   const [rawCurrentSearchResults, setRawCurrentSearchResults] = useState([])
-  const [searchRouteObj, setSearchRouteObj] = useState({ type: '', path: '' })
+  const [totalTokenCount, setTotalTokenCount] = useState(0)
   useEffect(() => {
     socket = new WebSocket('wss://s1.xrplmeta.org', 'tokens')
 
@@ -168,8 +133,10 @@ const SearchResults = ({
     })
 
     socket.addEventListener('message', (event) => {
-      // console.log('Got message from server:', event.data)
+      console.log('Got message from server:', event.data)
       const results = JSON.parse(event.data).result.tokens
+      setTotalTokenCount(JSON.parse(event.data).result.count)
+      console.log(totalTokenCount)
       const filteredResults = results.filter(
         (result) =>
           result.metrics.trustlines > 50 &&
@@ -190,7 +157,7 @@ const SearchResults = ({
       name_like: currentSearchValue,
       trust_level: [1, 2, 3],
       sort_by: 'holders',
-      limit: 50,
+      limit: 20,
     }
     if (socket.readyState === socket.OPEN && currentSearchValue !== '') {
       socket.send(JSON.stringify(command))
@@ -198,20 +165,11 @@ const SearchResults = ({
     if (currentSearchValue === '') {
       setRawCurrentSearchResults([])
     }
-
-    getRoute(currentSearchValue, xrplSocket).then((routeObj) => {
-      if (routeObj) {
-        setSearchRouteObj(routeObj)
-      } else {
-        setSearchRouteObj({ type: '', path: '' })
-      }
-    })
   }, [currentSearchValue, socket])
 
   const onLinkClick = () => {
     setCurrentSearchInput('')
     setRawCurrentSearchResults([])
-    setSearchRouteObj({ type: '', path: '' })
   }
   return (
     <div className="search-results-menu">
@@ -219,13 +177,6 @@ const SearchResults = ({
         <div className="search-results-header">
           Tokens ({rawCurrentSearchResults.length})
         </div>
-      )}
-      {searchRouteObj && searchRouteObj.path !== '' && (
-        <SearchResultBarNonToken
-          type={searchRouteObj.type}
-          path={searchRouteObj.path}
-          onClick={onLinkClick}
-        />
       )}
       {rawCurrentSearchResults.map((searchResultContent) => (
         <SearchResultBar
