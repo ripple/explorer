e5b9e0288f9324bbd18bb2d003b13b016364caea
Mayukha Vadari <mvadari@gmail.com>
2024-10-31 22:18:07 -0400

    migrate Details tests

--- src/containers/shared/components/TokenTableRow.tsx
+++ src/containers/shared/components/TokenTableRow.tsx
@@ -6,7 +6,7 @@ export interface TokenTableRowProps {
 export const TokenTableRow = (props: TokenTableRowProps) => {
   const { label, value } = props
   return (
-    <tr className="row">
+    <tr className="row" title="row">
       <td className="col1">{label}</td>
       <td className="col2">{value}</td>
     </tr>
