d69b53a4e95ec5751cd945b5f2ff77d9cd71b34b
Ashray Chowdhry <achowdhry@ripple.com>
2024-11-05 13:25:35 -0500

    cache all filtered tokens

--- server/routes/v1/tokens.js
+++ server/routes/v1/tokens.js
@@ -7,34 +7,39 @@ const cachedTokenSearchList = { tokens: [], last_updated: null }
 async function fetchXRPLMetaTokens(offset) {
   log.info('caching tokens from XRPLMeta')
   return axios
-    .get(`${process.env.XRPL_META_URL}/tokens`, {
-      params: {
-        trust_level: [1, 2, 3],
-        sort_by: 'holders',
-        limit: 1000,
-        offset: 10000,
+    .get(
+      `${process.env.XRPL_META_URL}/tokens?trust_level=1&trust_level=2&trust_level=3`,
+      {
+        params: {
+          sort_by: 'holders',
+          offset,
+          limit: 1000,
+        },
       },
-    })
-    .then((resp) => resp.data.tokens)
+    )
+    .then((resp) => resp.data)
     .catch((e) => log.error(e))
 }
 
 async function cacheXRPLMetaTokens() {
-  const offset = 10000
+  let offset = 0
   let tokenPage = []
-  // const allTokensFetched = []
-  // do {
-  //   tokenPage = await fetchXRPLMetaTokens(offset)
-  //   await sleep(1000)
-  //   log.info(`tokenPage length: ${tokenPage.length}`)
-  //   log.info(`offset: ${offset}`)
-  //   allTokensFetched.push(...tokenPage)
-  //   offset += 1000
-  // } while (tokenPage.length > 0)
-  // log.info(`Retrieved all tokens: ${allTokensFetched.length}`)
-  tokenPage = await fetchXRPLMetaTokens(offset)
-  log.info(`tokenPage length: ${tokenPage.length}`)
-  const filterredTokens = tokenPage.filter(
+  const allTokensFetched = []
+
+  const XRPLMetaTokensData = await fetchXRPLMetaTokens(0)
+  let { count } = XRPLMetaTokensData
+  while (count > 0) {
+    tokenPage = await fetchXRPLMetaTokens(offset)
+    await sleep(1000)
+    log.info(`tokenPage length: ${tokenPage.tokens.length}`)
+    log.info(`count: ${count}`)
+    allTokensFetched.push(...tokenPage.tokens)
+    offset += 1000
+    count -= 1000
+  }
+
+  log.info(`Retrieved all tokens: ${allTokensFetched.length}`)
+  const filterredTokens = allTokensFetched.filter(
     (result) =>
       result.metrics.trustlines > 50 &&
       result.metrics.holders > 50 &&
