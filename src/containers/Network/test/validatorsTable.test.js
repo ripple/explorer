c549b2c52c2ee1f8d1f72a7b67bf4093ca2379db
Phu Pham <ppham@ripple.com>
2024-12-11 11:31:50 -0800

    add tab tests

--- src/containers/Network/test/validatorsTable.test.js
+++ src/containers/Network/test/validatorsTable.test.js
@@ -28,4 +28,24 @@ describe('Validators table', () => {
     expect(wrapper.find('tr').length).toBe(validators.length + 1)
     wrapper.unmount()
   })
+
+  it('renders uptime tab', () => {
+    const tab = 'uptime'
+    const wrapper = createWrapper({ validators, metrics, tab })
+    expect(wrapper.find('.uptime-tab').length).toBe(1)
+    expect(wrapper.find('td.h1').at(0).text().trim()).toBe('1.00000')
+    expect(wrapper.find('td.h24').at(0).text().trim()).toBe('0.91729*')
+    expect(wrapper.find('td.d30').at(0).text().trim()).toBe('0.98468*')
+    wrapper.unmount()
+  })
+
+  it('renders voting tab', () => {
+    const tab = 'voting'
+    const wrapper = createWrapper({ validators, metrics, tab })
+    expect(wrapper.find('.voting-tab').length).toBe(1)
+    expect(wrapper.find('td.base').at(0).text().trim()).toContain('1.00')
+    expect(wrapper.find('td.owner').at(0).text().trim()).toContain('0.20')
+    expect(wrapper.find('td.base_fee').at(0).text().trim()).toContain('0.00001')
+    wrapper.unmount()
+  })
 })
