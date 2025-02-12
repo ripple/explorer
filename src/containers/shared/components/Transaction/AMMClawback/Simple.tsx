4d3833138b8ac0b765ad8a99fb45fd1df519171f
Phu Pham <ppham@ripple.com>
2025-02-12 15:30:31 -0500

    add amm_clawback

---
+++ src/containers/shared/components/Transaction/AMMClawback/Simple.tsx
@@ -0,0 +1,29 @@
+import { useTranslation } from 'react-i18next'
+import { TransactionSimpleProps } from '../types'
+import { SimpleRow } from '../SimpleRow'
+import { Account } from '../../Account'
+import { Amount } from '../../Amount'
+
+export const Simple = ({ data }: TransactionSimpleProps) => {
+  const { t } = useTranslation()
+  const { amount2, amount, holder } = data.instructions
+  return (
+    <>
+      {holder && (
+        <SimpleRow label={t('holder')} data-test="holder">
+          <Account account={holder} />
+        </SimpleRow>
+      )}
+      {amount && (
+        <SimpleRow label={t('asset1')} data-test="asset1">
+          <Amount value={amount} displayIssuer />
+        </SimpleRow>
+      )}
+      {amount2 && (
+        <SimpleRow label={t('asset2')} data-test="asset2">
+          <Amount value={amount2} displayIssuer />
+        </SimpleRow>
+      )}
+    </>
+  )
+}
---
+++ src/containers/shared/components/Transaction/AMMClawback/TableDetail.tsx
@@ -0,0 +1,33 @@
+import { Trans, useTranslation } from 'react-i18next'
+import { Amount } from '../../Amount'
+import { Account } from '../../Account'
+import { TransactionTableDetailProps } from '../types'
+
+export const TableDetail = ({ instructions }: TransactionTableDetailProps) => {
+  const { t } = useTranslation()
+  const { amount2, amount, holder } = instructions
+  if (amount2) {
+    return (
+      <div className="ammClawback">
+        <Trans i18nKey="action_from_and">
+          <span className="label">{t('claws_back')}</span>
+          <Amount value={amount} displayIssuer />
+          and
+          <Amount value={amount2} displayIssuer />
+          from
+          <Account account={holder} />
+        </Trans>
+      </div>
+    )
+  }
+  return (
+    <div className="ammClawback">
+      <Trans i18nKey="action_from">
+        <span className="label">{t('claws_back')}</span>
+        <Amount value={amount} displayIssuer />
+        from
+        <Account account={holder} />
+      </Trans>
+    </div>
+  )
+}
