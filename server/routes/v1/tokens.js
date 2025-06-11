f27a24168e79892de97d193f2b9e2f7518141699
achowdhry-ripple <achowdhry@ripple.com>
2025-06-11 18:56:38 -0400

    fix: allow token search with both currency and parsed currencies  (#1196)
    ## High Level Overview of Change
    
    <!--
    Please include a summary/list of the changes.
    If too broad, please consider splitting into multiple PRs.
    -->
    
    ### Context of Change
    
    <!--
    Please include the context of a change.
    If a bug fix, when was the bug introduced? What was the behavior?
    If a new feature, why was this architecture chosen? What were the
    alternatives?
    If a refactor, how is this better than the previous implementation?
    
    If there is a design document for this feature, please link it here.
    -->
    
    ### Type of Change
    
    <!--
    Please check relevant options, delete irrelevant ones.
    -->
    
    - [ ] Bug fix (non-breaking change which fixes an issue)
    - [ ] New feature (non-breaking change which adds functionality)
    - [ ] Breaking change (fix or feature that would cause existing
    functionality to not work as expected)
    - [ ] Refactor (non-breaking change that only restructures code)
    - [ ] Tests (You added tests for code that already exists, or your new
    feature included in this PR)
    - [ ] Documentation Updates
    - [ ] Translation Updates
    - [ ] Release
    
    ### Codebase Modernization
    
    <!--
    In an effort to modernize the codebase, you should convert the files
    that you work with to React Hooks and TypeScript,
    and update tests to use the React Testing Library instead of Enzyme. If
    this is not possible (e.g. it's too many
    changes, touching too many files, etc.) please explain why here.
    -->
    
    - [ ] Updated files to React Hooks
    - [ ] Updated files to TypeScript
    - [ ] Updated tests to React Testing Library
    
    ## Before / After
    
    <!--
    If just refactoring / back-end changes, this can be just an in-English
    description of the change at a technical level.
    If a UI change, screenshots should be included.
    -->
    
    ## Test Plan
    
    <!--
    Please describe the tests that you ran to verify your changes and
    provide instructions so that others can reproduce.
    -->
    
    <!--
    ## Future Tasks
    For future tasks related to PR.
    -->

--- server/routes/v1/tokens.js
+++ server/routes/v1/tokens.js
@@ -54,7 +54,7 @@ async function tempFetchOndo(offset) {
     .get(
       `https://${process.env.XRPL_META_URL}/tokens?name_like=rHuiXXjHLpMP8ZE9sSQU5aADQVWDwv6h5p`,
     )
-    .then((resp) => resp.data.tokens[0])
+    .then((resp) => resp.data)
     .catch((e) => {
       log.error(e)
       return { count: 0 }
@@ -85,16 +85,14 @@ async function cacheXRPLMetaTokens() {
   )
 
   // TEMP: Manually add OUSG for Ondo until token is KYCed
-  const ondoToken = await tempFetchOndo()
-  ondoToken.currency = parseCurrency(ondoToken.currency)
-  cachedTokenSearchList.tokens.push(ondoToken)
-
+  const ondoTokens = await tempFetchOndo()
+  cachedTokenSearchList.tokens.push(...ondoTokens.tokens)
   cachedTokenSearchList.last_updated = Date.now()
 
   // nonstandard from XRPLMeta, check for hex codes in currencies and store parsed
-  cachedTokenSearchList.tokens.map((token) => ({
+  cachedTokenSearchList.tokens = cachedTokenSearchList.tokens.map((token) => ({
     ...token,
-    currency: parseCurrency(token.currency),
+    parsedCurrency: parseCurrency(token.currency),
   }))
 }
 
@@ -114,6 +112,7 @@ function queryTokens(tokenList, query) {
   return tokenList.filter(
     (token) =>
       token.currency?.toLowerCase().includes(sanitizedQuery) ||
+      token.parsedCurrency?.toLowerCase().includes(sanitizedQuery) ||
       token.meta?.token?.name?.toLowerCase().includes(sanitizedQuery) ||
       token.meta?.issuer?.name?.toLowerCase().includes(sanitizedQuery) ||
       token.issuer?.toLowerCase().startsWith(sanitizedQuery),
