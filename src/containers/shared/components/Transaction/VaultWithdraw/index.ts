c7d3c90b4db046d1e458c50e56fc48ca9706002f
Omar Khan <khancodegt@gmail.com>
2025-07-30 12:57:37 -0400

    add VaultWithdrawTableDetail

--- src/containers/shared/components/Transaction/VaultWithdraw/index.ts
+++ src/containers/shared/components/Transaction/VaultWithdraw/index.ts
@@ -5,10 +5,12 @@ import {
 } from '../types'
 import { Description } from './Description'
 import { Simple } from './Simple'
+import { TableDetail } from './TableDetail'
 
 export const VaultWithdrawTransaction: TransactionMapping = {
   Description,
   Simple,
+  TableDetail,
   action: TransactionAction.SEND,
   category: TransactionCategory.OTHER,
 }
