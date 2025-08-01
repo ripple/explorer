21b85011a9a00dae5c9d6e8c97103c2931b1894d
Omar Khan <khancodegt@gmail.com>
2025-07-31 21:36:28 -0400

    use VaultCreate type from xrpl

--- src/containers/shared/components/Transaction/VaultCreate/Description.tsx
+++ src/containers/shared/components/Transaction/VaultCreate/Description.tsx
@@ -1,20 +1,34 @@
 import { Trans } from 'react-i18next'
+import type { VaultCreate } from 'xrpl'
 import { TransactionDescriptionProps } from '../types'
-import { VaultCreate } from './types'
 import { Account } from '../../Account'
 import Currency from '../../Currency'
+import { MPTokenLink } from '../../MPTokenLink'
 
 export const Description = ({
   data,
 }: TransactionDescriptionProps<VaultCreate>) => {
   const { tx } = data
   const { Account: account, Asset: asset } = tx
+  // @ts-expect-error -- necessary to check for MPT
+  const mptIssuanceId = asset.mpt_issuance_id
+  const isMPT = mptIssuanceId != null
   return (
     <Trans
       i18nKey="account_creates_vault"
       components={{
         Account: <Account account={account} />,
-        Asset: <Currency currency={asset.currency} issuer={asset.issuer} />,
+        Asset: isMPT ? (
+          <MPTokenLink tokenID={mptIssuanceId} />
+        ) : (
+          <Currency
+            // @ts-expect-error -- this is fine
+            currency={asset.currency}
+            // @ts-expect-error -- this is fine
+            issuer={asset.issuer}
+            isMPT={isMPT}
+          />
+        ),
       }}
     />
   )
--- src/containers/shared/components/Transaction/VaultCreate/Simple.tsx
+++ src/containers/shared/components/Transaction/VaultCreate/Simple.tsx
@@ -1,10 +1,11 @@
 import { useTranslation } from 'react-i18next'
+import type { VaultCreate } from 'xrpl'
 import { TransactionSimpleComponent, TransactionSimpleProps } from '../types'
-import { VaultCreate } from './types'
 import { SimpleRow } from '../SimpleRow'
 import Currency from '../../Currency'
 import { isValidJsonString } from '../../../utils'
 import { JsonView } from '../../JsonView'
+import { MPTokenLink } from '../../MPTokenLink'
 
 export const Simple: TransactionSimpleComponent = (
   props: TransactionSimpleProps<VaultCreate>,
@@ -19,10 +20,23 @@ export const Simple: TransactionSimpleComponent = (
     WithdrawalPolicy,
     DomainID,
   } = data.instructions
+  // @ts-expect-error -- necessary to check for MPT
+  const mptIssuanceId = Asset.mpt_issuance_id
+  const isMPT = mptIssuanceId != null
   return (
     <>
       <SimpleRow label={t('asset')} data-testid="asset">
-        <Currency currency={Asset.currency} issuer={Asset.issuer} />
+        {isMPT ? (
+          <MPTokenLink tokenID={mptIssuanceId} />
+        ) : (
+          <Currency
+            // @ts-expect-error -- this is fine
+            currency={Asset.currency}
+            // @ts-expect-error -- this is fine
+            issuer={Asset.issuer}
+            isMPT={isMPT}
+          />
+        )}
       </SimpleRow>
       {AssetsMaximum && (
         <SimpleRow label={t('assets_maximum')} data-testid="assets_maximum">
