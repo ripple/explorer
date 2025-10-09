ded86a1800d98d9cc218eb57ec3e19058c6e8108
Phu Pham <ppham@ripple.com>
2025-10-09 13:28:53 -0400

    Add expand all and collapse all button for transaction raw tab

--- src/containers/shared/components/JsonView/JsonView.tsx
+++ src/containers/shared/components/JsonView/JsonView.tsx
@@ -1,16 +1,51 @@
+import { useState } from 'react'
 import ReactJson from 'react18-json-view'
+import { useTranslation } from 'react-i18next'
 
 import './json-view.scss'
+import CollapseAllIcon from '../../images/collapse_all.svg'
+import ExpandAllIcon from '../../images/expand_all.svg'
 
-export const JsonView = ({ data }: { data: any }) => (
-  <ReactJson
-    src={data}
-    collapsed={5}
-    collapseStringsAfterLength={65}
-    customizeNode={(params) => {
-      if (params.node === undefined)
-        return { className: 'json-view--undefined' }
-      return undefined
-    }}
-  />
-)
+interface JsonViewProps {
+  data: any
+  showExpandButton?: boolean
+}
+
+export const JsonView = ({ data, showExpandButton = false }: JsonViewProps) => {
+  const { t } = useTranslation()
+  const [isExpanded, setIsExpanded] = useState(false)
+
+  const handleExpandToggle = () => {
+    setIsExpanded(!isExpanded)
+  }
+
+  const Icon = isExpanded ? CollapseAllIcon : ExpandAllIcon
+
+  return (
+    <div className="json-view-container">
+      {showExpandButton && (
+        <div className="json-view-controls">
+          <button
+            type="button"
+            className="json-view-expand-button"
+            onClick={handleExpandToggle}
+            aria-label={isExpanded ? t('collapse_all') : t('expand_all')}
+          >
+            <Icon width={24} height={24} />
+            {isExpanded ? t('collapse_all') : t('expand_all')}
+          </button>
+        </div>
+      )}
+      <ReactJson
+        src={data}
+        collapsed={isExpanded ? false : 5}
+        collapseStringsAfterLength={65}
+        customizeNode={(params) => {
+          if (params.node === undefined)
+            return { className: 'json-view--undefined' }
+          return undefined
+        }}
+      />
+    </div>
+  )
+}
