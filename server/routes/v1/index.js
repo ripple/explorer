ee317e40baf75aebad3016b7f48706685cb949fb
Phu Pham <ppham@ripple.com>
2025-10-20 10:55:14 -0400

    fix tokens import

--- server/routes/v1/index.js
+++ server/routes/v1/index.js
@@ -1,7 +1,7 @@
 const api = require('express').Router()
 const getHealth = require('./health')
 const getCurrentMetrics = require('./currentMetrics')
-const getTokens = require('./tokens')
+const { getTokensSearch, getAllTokens } = require('./tokens')
 
 api.use('/healthz', (_req, res) => {
   res.status(200).send('success')
@@ -10,8 +10,8 @@ if (process.env.VITE_ENVIRONMENT !== 'custom') {
   // these require a single hardcoded rippled node to connect to
   api.use('/health', getHealth)
   api.use('/metrics', getCurrentMetrics)
-  api.use('/tokens/search/:query', getTokens.getTokensSearch)
-  api.use('/tokens', getTokens.getAllTokens)
+  api.use('/tokens/search/:query', getTokensSearch)
+  api.use('/tokens', getAllTokens)
 }
 
 module.exports = api
