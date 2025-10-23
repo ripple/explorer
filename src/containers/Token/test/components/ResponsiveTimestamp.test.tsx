799cd513bb5f1edc915da94b0354aee25d249eb2
Ashray Chowdhry <achowdhry@ripple.com>
2025-10-23 18:06:48 -0400

    lint

--- src/containers/Token/test/components/ResponsiveTimestamp.test.tsx
+++ src/containers/Token/test/components/ResponsiveTimestamp.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen } from '@testing-library/react'
+import { render } from '@testing-library/react'
 import { ResponsiveTimestamp } from '../../components/ResponsiveTimestamp/ResponsiveTimestamp'
 
 describe('ResponsiveTimestamp Component', () => {
