9e66b714d69b902b216c584f88289254fbc69908
Ashray Chowdhry <achowdhry@ripple.com>
2025-10-27 13:29:37 -0400

    test fix

--- src/containers/shared/components/EmptyStateMessage/tests/index.test.tsx
+++ src/containers/shared/components/EmptyStateMessage/tests/index.test.tsx
@@ -6,7 +6,6 @@ describe('EmptyStateMessage', () => {
     const testMessage = 'No data available'
     const wrapper = mount(<EmptyStateMessage message={testMessage} />)
     expect(wrapper.find('.empty-state-message').length).toEqual(1)
-    expect(wrapper.find('.empty-state-icon').length).toEqual(1)
     expect(wrapper.find('.empty-state-text').text()).toEqual(testMessage)
     wrapper.unmount()
   })
