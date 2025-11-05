a83db2b46d848dbf39e34ed14c1b85c0d17d0ce5
Chenna Keshava B S <ckeshavabs@gmail.com>
2025-11-05 16:32:31 +0530

    revert changes pertaining to LedgerCountBanner; This is a temporary change which is slated to be removed in a few days

--- src/containers/Ledgers/index.tsx
+++ src/containers/Ledgers/index.tsx
@@ -3,18 +3,14 @@ import { Helmet } from 'react-helmet-async'
 import { useTranslation } from 'react-i18next'
 import { LedgerMetrics } from './LedgerMetrics'
 import { Ledgers } from './Ledgers'
-import { LedgerCountdownBanner } from './LedgerCountdownBanner'
 import { useAnalytics } from '../shared/analytics'
 import { SelectedValidatorProvider } from './useSelectedValidator'
-import { StreamsProvider, useStreams } from '../shared/components/Streams'
+import { StreamsProvider } from '../shared/components/Streams'
 import { VHSValidatorsProvider } from '../shared/components/VHSValidators'
 
 export const LedgersPage = () => {
   const { trackScreenLoaded } = useAnalytics()
   const [paused, setPaused] = useState(false)
-  const [currentLedgerIndex, setCurrentLedgerIndex] = useState<
-    number | undefined
-  >(undefined)
 
   useEffect(() => {
     trackScreenLoaded()
@@ -25,16 +21,6 @@ export const LedgersPage = () => {
 
   const pause = () => setPaused(!paused)
   const { t } = useTranslation()
-  const { ledgers } = useStreams()
-
-  // Extract current ledger index from the ledgers record
-  useEffect(() => {
-    const keys = Object.keys(ledgers)
-    if (keys.length > 0) {
-      const maxLedger = Math.max(...keys.map((k) => Number(k)))
-      setCurrentLedgerIndex(maxLedger)
-    }
-  }, [ledgers])
 
   return (
     <div className="ledgers-page">
@@ -42,7 +28,6 @@ export const LedgersPage = () => {
       <StreamsProvider>
         <VHSValidatorsProvider>
           <SelectedValidatorProvider>
-            <LedgerCountdownBanner currentLedger={currentLedgerIndex} />
             <LedgerMetrics onPause={() => pause()} paused={paused} />
             <Ledgers paused={paused} />
           </SelectedValidatorProvider>
