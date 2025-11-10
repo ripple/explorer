e084ccc8e94830ef794bbdaffc5b39a900904885
Phu Pham <ppham@ripple.com>
2025-11-10 13:21:00 -0500

    fix test

--- src/containers/shared/components/Transaction/LoanSet/test/LoanSetTableDetail.test.tsx
+++ src/containers/shared/components/Transaction/LoanSet/test/LoanSetTableDetail.test.tsx
@@ -10,7 +10,7 @@ describe('LoanSetTableDetail', () => {
     const wrapper = createWrapper(LoanSet)
 
     expect(wrapper.find('.loan-set')).toHaveText(
-      'Loan Broker ID: 7B3AF305C92293AF3F01088298E354E7B649F963427FA4B7F5414EF1383CB80BRequest: \uE9000.00001 XRP from rH4absn9JcB8m943YRMNJpuR9HQs56hkr8Rates: interest rate 0.50%, late interest rate 1.00%, overpayment fee 0.50%Fees: loan origination fee \uE9000.000001 XRP, loan service fee \uE9000.0000001 XRPTerms: 12 payment total, payment interval 30d, grace period 7d',
+      'Loan Broker ID: 7B3AF305C92293AF3F01088298E354E7B649F963427FA4B7F5414EF1383CB80BRequest: 0.00001 XRP from rH4absn9JcB8m943YRMNJpuR9HQs56hkr8Rates: Interest Rate 0.50%, Late Interest Rate 1.00%, Overpayment Fee 0.50%Fees: Loan Origination Fee 0.000001 XRP, Loan Service Fee 0.0000001 XRPTerms: 12 Payment Total, Payment Interval 30d, Grace Period 7d',
     )
 
     wrapper.unmount()
