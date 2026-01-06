e6c5d0e26e5c0449b2975c62ca046a05129fa310
Mayukha Vadari <mvadari@ripple.com>
2026-01-06 10:25:15 -0500

    refactor: convert new TS files to async/await (#1270)
    ## High Level Overview of Change
    
    This PR is a fast follow from #1269 to refactor the files converted to
    TS to use proper async/await structure instead of `then` structure.
    
    There is no functionality change, only refactors.
    
    ### Context of Change
    
    Best practices, easier to read/write
    
    ### Type of Change
    
    - [x] Refactor (non-breaking change that only restructures code)
    
    ### Codebase Modernization
    
    Already done ;)
    
    ## Before / After
    
    No changes
    
    ## Test Plan
    
    CI passes.

--- src/rippled/quorum.ts
+++ src/rippled/quorum.ts
@@ -5,26 +5,27 @@ import type { ExplorerXrplClient } from '../containers/shared/SocketContext'
 
 const log = logger({ name: 'quorum' })
 
-const getQuorum = (rippledSocket: ExplorerXrplClient): Promise<number> => {
+const getQuorum = async (
+  rippledSocket: ExplorerXrplClient,
+): Promise<number> => {
   log.info(`fetching server_info from rippled`)
 
-  return getServerInfo(rippledSocket)
-    .then((result) => {
-      if (result === undefined || result.info === undefined) {
-        throw new Error('Undefined result from getServerInfo()', 500)
-      }
+  try {
+    const result = await getServerInfo(rippledSocket)
+    if (result === undefined || result.info === undefined) {
+      throw new Error('Undefined result from getServerInfo()', 500)
+    }
 
-      const quorum = result.info.validation_quorum
-      if (quorum === undefined) {
-        throw new Error('Undefined validation_quorum', 500)
-      }
+    const quorum = result.info.validation_quorum
+    if (quorum === undefined) {
+      throw new Error('Undefined validation_quorum', 500)
+    }
 
-      return quorum
-    })
-    .catch((error: any) => {
-      log.error(error.toString())
-      throw error
-    })
+    return quorum
+  } catch (error: any) {
+    log.error(error.toString())
+    throw error
+  }
 }
 
 export default getQuorum
