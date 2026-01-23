4b8cadacf06b972e8b8978acd0cb8ed847abea12
Chenna Keshava B S <ckeshavabs@gmail.com>
2026-01-23 14:01:48 -0800

    remove debug console.log messages

--- src/rippled/lib/rippled.ts
+++ src/rippled/lib/rippled.ts
@@ -841,11 +841,8 @@ const getMPTHolders = (rippledSocket, mptIssuanceId, limit = 20, marker = '') =>
     }
 
     if (resp.error_message) {
-      console.log('error response for the getMPTHolders request: ', resp)
       throw new Error(resp.error_message, 500)
     }
-
-    console.log('response for the getMPTHolders request: ', resp)
 
     return resp
   })
